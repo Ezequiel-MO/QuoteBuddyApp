3361a7a2255fca73707f496a425707b238b43cec
Ezequiel <oliverm316@gmail.com>
2022-08-23 09:42:17 +0200

    minor fix

--- src/screens/projects/ProjectList/ProjectListItem.jsx
+++ src/screens/projects/ProjectList/ProjectListItem.jsx
@@ -1,10 +1,29 @@
 import { useNavigate } from "react-router-dom";
 import { Icon } from "@iconify/react";
 import accounting from "accounting";
-import { removeItemFromList } from "../../../helper/RemoveItemFromList";
+import baseAPI from "../../../axios/axiosConfig";
+import { toast } from "react-toastify";
+import { toastOptions } from "../../../helper/toast";
 
 const ProjectListItem = ({ project, handleRecycleProject }) => {
   const navigate = useNavigate();
+  const removeItemFromList = async (endpoint, ID) => {
+    const confirmDelete = window.confirm(
+      `Are you sure you want to delete this ${endpoint.slice(0, -1)}  ?`
+    );
+    if (confirmDelete) {
+      try {
+        await baseAPI.delete(`v1/${endpoint}/${ID}`);
+        toast.success("Deleted successfully", toastOptions);
+        navigate(`/app/${endpoint.slice(0, -1)}`);
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      toast.warn("Could not delete Item successfully", toastOptions);
+    }
+  };
+
   return (
     <tbody>
       <tr className="mb-2 p-1 bg-gray-900 hover:bg-green-100 hover:text-black-50 rounded-md text-white-50">
