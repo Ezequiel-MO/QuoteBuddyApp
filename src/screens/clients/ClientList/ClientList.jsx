0a1631223420d52bcae4bf17329ea1592fd6b26b
Ezequiel <oliverm316@gmail.com>
2022-09-16 13:31:03 +0200

    Client List search bar

--- src/screens/clients/ClientList/ClientList.jsx
+++ src/screens/clients/ClientList/ClientList.jsx
@@ -1,19 +1,39 @@
-import { useState } from 'react'
-import { Icon } from '@iconify/react'
+import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import CountryFilter from '../../../ui/filters/CountryFilter'
 import Spinner from '../../../ui/spinner/Spinner'
 import ClientListItem from './ClientListItem'
 import useGetClients from '../../../hooks/useGetClients'
 import TableHeaders from '../../../ui/TableHeaders'
+import SearchInput from '../../../ui/inputs/SearchInput'
+import { prepareDataForValidation } from 'formik'
 
 const ClientList = () => {
   const navigate = useNavigate()
   const [client] = useState({})
   const [country, setCountry] = useState('')
+  const [searchItem, setSearchItem] = useState('')
   const { clients, setClients, isLoading } = useGetClients(country)
+  const [foundClients, setFoundClients] = useState([])
 
-  const clientList = clients?.map((client) => (
+  useEffect(() => {
+    setFoundClients(clients)
+  }, [clients])
+
+  const filterList = (e) => {
+    setSearchItem(e.target.value)
+    const result = clients.filter(
+      (data) =>
+        data.firstName.includes(e.target.value) ||
+        data.familyName.includes(e.target.value)
+    )
+    setFoundClients(result)
+    if (searchItem === '') {
+      setFoundClients(clients)
+    }
+  }
+
+  const clientList = foundClients?.map((client) => (
     <ClientListItem
       key={client._id}
       client={client}
@@ -27,18 +47,19 @@ const ClientList = () => {
       <div className='flex flex-col sm:flex-row sm:items-end items-start sm:space-x-6 mb-4 mr-8 ml-8'>
         <div className='flex flex-col w-full'>
           <h1 className='text-2xl'>Client List</h1>
-          <div className='flex flex-row justify-start'>
+          <div className='flex flex-row justify-start items-center'>
             <div>
-              <CountryFilter setCountry={setCountry} />
+              <CountryFilter setCountry={setCountry} country={country} />
             </div>
             <button
               onClick={() =>
                 navigate('/app/client/specs', { state: { client } })
               }
-              className='focus:scale-110 hover:animate-pulse bg-transparent hover:bg-orange-50 text-white-100 uppercase font-semibold hover:text-black-50 py-2 px-4 border border-orange-50 hover:border-transparent rounded'
+              className='mx-5 focus:scale-110 hover:animate-pulse bg-transparent hover:bg-orange-50 text-white-100 uppercase font-semibold hover:text-black-50 py-2 px-4 border border-orange-50 hover:border-transparent rounded'
             >
               Create New Client
             </button>
+            <SearchInput searchItem={searchItem} filterList={filterList} />
           </div>
         </div>
       </div>
--- src/ui/filters/CountryFilter.jsx
+++ src/ui/filters/CountryFilter.jsx
@@ -1,30 +1,28 @@
-import useGetCountries from "../../hooks/useGetCountries";
+import useGetCountries from '../../hooks/useGetCountries'
 
-const CountryFilter = ({ setCountry }) => {
-  const { countries } = useGetCountries();
+const CountryFilter = ({ setCountry, country }) => {
+  const { countries: options } = useGetCountries()
   return (
-    <div className="w-11/12 max-w-sm my-2 ml-0 mr-0">
+    <div className='w-60 max-w-sm my-2 ml-0 mr-0'>
       <form>
-        <div className="flex items-center gap-8">
-          <label className="text-xl text-gray-100" htmlFor="country">
-            Filter by Country
-          </label>
+        <div className='flex items-center gap-2'>
           <select
-            id="country"
-            className="flex-1 py-1 px-2 border-0 rounded-xl bg-green-50 text-center cursor-pointer"
+            id='country'
+            className='flex-1 py-1 px-2 border-0 rounded-xl bg-green-50 text-center cursor-pointer'
+            value={country}
             onChange={(e) => setCountry(e.target.value)}
           >
-            <option value="none">--- Select a country ---</option>
-            {countries?.map((country) => (
-              <option key={country._id} value={country.accessCode}>
-                {country.name}
+            <option value='none'>--- Filter by country ---</option>
+            {options?.map((option) => (
+              <option key={option._id} value={option.accessCode}>
+                {option.name}
               </option>
             ))}
           </select>
         </div>
       </form>
     </div>
-  );
-};
+  )
+}
 
-export default CountryFilter;
+export default CountryFilter
