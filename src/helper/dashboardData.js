b292e7ef55dcdfa56b52ca6b806566af872a5675
Ezequiel <oliverm316@gmail.com>
2022-11-11 09:57:04 +0100

    Sidebar Dashboard restyling

--- src/helper/dashboardData.js
+++ src/helper/dashboardData.js
@@ -1,11 +1,15 @@
 export const dashboardData = [
-  { title: 'Hotels', route: 'hotel' },
-  { title: 'Restaurants', route: 'restaurant' },
-  { title: 'Events', route: 'event' },
-  { title: 'Transfers', route: 'transfer' },
-  { title: 'Clients', route: 'client' },
-  { title: 'Locations', route: 'location' },
-  { title: 'Account Managers', route: 'accManager' },
-  { title: 'Countries', route: 'country' },
-  { title: 'Invoices', route: 'invoice' }
+  { title: 'Hotels', route: 'hotel', icon: 'bxs:hotel' },
+  { title: 'Restaurants', route: 'restaurant', icon: 'bx:restaurant' },
+  { title: 'Events', route: 'event', icon: 'ic:baseline-event-available' },
+  { title: 'Transfers', route: 'transfer', icon: 'cil:bus-alt' },
+  { title: 'Clients', route: 'client', icon: 'mdi:handshake-outline' },
+  { title: 'Locations', route: 'location', icon: 'akar-icons:location' },
+  { title: 'Account Managers', route: 'accManager', icon: 'akar-icons:person' },
+  { title: 'Countries', route: 'country', icon: 'gis:search-country' },
+  {
+    title: 'Invoices',
+    route: 'invoice',
+    icon: 'ant-design:euro-circle-outlined'
+  }
 ]
--- src/screens/dashboard/DashboardSidebar.jsx
+++ src/screens/dashboard/DashboardSidebar.jsx
@@ -1,21 +1,29 @@
-import { useNavigate } from "react-router-dom";
-import { dashboardData } from "../../helper/dashboardData";
+import { Icon } from '@iconify/react'
+import { useNavigate } from 'react-router-dom'
+import { dashboardData } from '../../helper/dashboardData'
 
 const DashboardSidebar = () => {
-  const navigate = useNavigate();
+  const navigate = useNavigate()
   return (
-    <ul className="indent-6 text-white-100">
-      {dashboardData.map(({ title, route }) => (
+    <ul className='indent-6 text-white-100 bg-black-100 h-fit mt-9 rounded'>
+      {dashboardData.map(({ title, route, icon }) => (
         <li
+          className='font-bold text-white-50 hover:text-orange-50 border-3 border-b last:border-none border-gray-100 p-2 flex items-center cursor-pointer'
+          onClick={() => navigate(`/app/${route}`)}
+        >
+          <Icon icon={icon} />
+          <p className='ml-2'>{title}</p>
+        </li>
+        /*  <li
           key={title}
           onClick={() => navigate(`/app/${route}`)}
           className="uppercase hover:text-orange-50 border-l-4 border-transparent hover:border-white-50 hover:cursor-pointer"
         >
           {title}
-        </li>
+        </li> */
       ))}
     </ul>
-  );
-};
+  )
+}
 
-export default DashboardSidebar;
+export default DashboardSidebar
