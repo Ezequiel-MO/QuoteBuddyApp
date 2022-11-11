89880318893b72892e84acca5dbfa333e3314d02
Ezequiel <oliverm316@gmail.com>
2022-11-11 11:05:01 +0100

    minor fix

--- src/screens/dashboard/DashboardSidebar.jsx
+++ src/screens/dashboard/DashboardSidebar.jsx
@@ -8,19 +8,13 @@ const DashboardSidebar = () => {
     <ul className='indent-6 text-white-100 bg-black-100 h-fit mt-9 rounded'>
       {dashboardData.map(({ title, route, icon }) => (
         <li
+          key={title}
           className='font-bold text-white-50 hover:text-orange-50 border-3 border-b last:border-none border-gray-100 p-2 flex items-center cursor-pointer'
           onClick={() => navigate(`/app/${route}`)}
         >
           <Icon icon={icon} />
           <p className='ml-2'>{title}</p>
         </li>
-        /*  <li
-          key={title}
-          onClick={() => navigate(`/app/${route}`)}
-          className="uppercase hover:text-orange-50 border-l-4 border-transparent hover:border-white-50 hover:cursor-pointer"
-        >
-          {title}
-        </li> */
       ))}
     </ul>
   )
