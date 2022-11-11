9152cb29f2a45253f2e0a00ac4e84f49039f5caa
Ezequiel <oliverm316@gmail.com>
2022-11-11 09:55:30 +0100

    useGetHotel implemented

---
+++ src/hooks/useGetHotel.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react'
+import { toast } from 'react-toastify'
+import baseAPI from '../axios/axiosConfig'
+import { toastOptions } from '../helper/toast'
+
+const useGetHotel = (id) => {
+  const [isLoading, setIsLoading] = useState(false)
+  const [hotel, setHotel] = useState({})
+
+  useEffect(() => {
+    const getHotel = async (id) => {
+      const url = `/v1/hotels/${id}`
+      setIsLoading(true)
+      try {
+        const response = await baseAPI.get(url)
+        setHotel(response.data.data.data)
+        setIsLoading(false)
+      } catch (error) {
+        toast.error(error, toastOptions)
+      }
+    }
+    getHotel(id)
+  }, [id])
+
+  return { hotel, isLoading }
+}
+
+export default useGetHotel
