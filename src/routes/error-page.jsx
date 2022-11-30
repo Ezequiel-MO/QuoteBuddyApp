03751e70d4460482122c8607b66b21c30708e13e
Ezequiel <oliverm316@gmail.com>
2022-11-30 17:07:49 +0100

    Not Found restyled

--- src/routes/error-page.jsx
+++ src/routes/error-page.jsx
@@ -1,17 +1,23 @@
-import { useRouteError } from 'react-router-dom'
+import { Link, useRouteError } from 'react-router-dom'
+import Button from '../ui/Button'
 
 export default function ErrorPage() {
   const error = useRouteError()
 
   return (
-    <main
-      id='error-page'
-      className='h-screen container mx-auto mt-4 md:mt-15 p-5 md:flex md:justify-center text-white-0 flex items-center justify-center '
-    >
-      <p className='text-8xl font-bold'>404 |</p>
-      <p className='text-4xl ml-10'>
-        <i>{error.statusText || error.message}</i>
-      </p>
-    </main>
+    <div className='h-screen flex flex-col items-center justify-center'>
+      <main
+        id='error-page'
+        className=' container mx-auto mt-4 md:mt-15 p-5 md:flex md:justify-center text-white-0 flex items-center justify-center '
+      >
+        <p className='text-8xl font-bold'>404 |</p>
+        <p className='text-4xl ml-10'>
+          <i>{error.statusText || error.message}</i>
+        </p>
+      </main>
+      <Button>
+        <Link to='/app'>Back to Home Page</Link>
+      </Button>
+    </div>
   )
 }
