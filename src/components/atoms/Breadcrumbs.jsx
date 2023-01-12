37e44b85cb8584c35d80cf0e04f4596b5e817dcf
Ezequiel <oliverm316@gmail.com>
2023-01-12 13:49:20 +0100

    airbnb styles loaded

--- src/components/atoms/Breadcrumbs.jsx
+++ src/components/atoms/Breadcrumbs.jsx
@@ -1,7 +1,6 @@
 import { Link, useLocation } from 'react-router-dom'
 
 export const Breadcrumbs = () => {
-  console.log('Breadcrumbs')
   const location = useLocation()
   let currentLink = ''
   const crumbs = location.pathname
