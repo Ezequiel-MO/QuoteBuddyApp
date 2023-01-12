ce2e0718fa9fbcc1e2aa8a57e998ee0721e3a28c
Ezequiel <oliverm316@gmail.com>
2023-01-12 16:37:39 +0100

    minor fix

--- src/components/atoms/Breadcrumbs.jsx
+++ src/components/atoms/Breadcrumbs.jsx
@@ -1,30 +1,30 @@
 /* eslint-disable react/no-array-index-key */
 /* eslint-disable react/react-in-jsx-scope */
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom'
 
 export function Breadcrumbs() {
-  console.log('Breadcrumbs');
-  const location = useLocation();
-  let currentLink = '';
-  const crumbs = location.pathname
-    .split('/')
-    .filter((crumb) => crumb !== '')
-    .map((crumb, index) => {
-      currentLink += `/${crumb}`;
-      return (
-        <div key={`${crumb}-${index}`}>
-          <Link to={currentLink}>
-            {crumb}
-            <span>
-              {index !== location.pathname.split('/').length - 2 && '/'}
-            </span>
-          </Link>
-        </div>
-      );
-    });
-  return (
-    <div className="flex flex-row items-center mt-3 font-semibold text-xl text-black-50">
-      {crumbs}
-    </div>
-  );
+	console.log('Breadcrumbs')
+	const location = useLocation()
+	let currentLink = ''
+	const crumbs = location.pathname
+		.split('/')
+		.filter((crumb) => crumb !== '')
+		.map((crumb, index) => {
+			currentLink += `/${crumb}`
+			return (
+				<div key={`${crumb}-${index}`}>
+					<Link to={currentLink}>
+						{crumb}
+						<span>
+							{index !== location.pathname.split('/').length - 2 && '/'}
+						</span>
+					</Link>
+				</div>
+			)
+		})
+	return (
+		<div className="flex flex-row items-center mt-3 font-semibold text-xl text-black-50">
+			{crumbs}
+		</div>
+	)
 }
--- src/screens/restaurants/RestaurantSpecs/RestaurantMasterForm.jsx
+++ src/screens/restaurants/RestaurantSpecs/RestaurantMasterForm.jsx
@@ -2,191 +2,191 @@ import { useState, useRef } from 'react'
 import * as Yup from 'yup'
 import { Form, Formik } from 'formik'
 import {
-  TextInput,
-  TextAreaInput,
-  CheckboxInput,
-  SelectInput
+	TextInput,
+	TextAreaInput,
+	CheckboxInput,
+	SelectInput
 } from '../../../ui'
 import { Icon } from '@iconify/react'
 import { useGetLocations } from '../../../hooks'
 import { Modal, Box, ImageList, ImageListItem } from '@mui/material'
 
 const RestaurantMasterForm = ({ submitForm, restaurant }) => {
-  const [open, setOpen] = useState(false)
-  const [imgList, setImgList] = useState([])
-  const [isUpdate, setIsUpdate] = useState(false)
-  const handleOpen = () => setOpen(true)
-  const handleClose = () => setOpen(false)
+	const [open, setOpen] = useState(false)
+	const [imgList, setImgList] = useState([])
+	const [isUpdate, setIsUpdate] = useState(false)
 
-  const fileInput = useRef()
-  const { locations } = useGetLocations()
+	const handleClose = () => setOpen(false)
 
-  const initialValues = {
-    name: restaurant?.name ?? '',
-    city: restaurant?.city ?? '',
-    longitude: restaurant?.location?.coordinates[1] ?? '',
-    latitude: restaurant?.location?.coordinates[0] ?? '',
-    price: restaurant?.price ?? '',
-    textContent: restaurant?.textContent ?? '',
-    isVenue: restaurant?.isVenue ?? false
-  }
+	const fileInput = useRef()
+	const { locations } = useGetLocations()
 
-  const update = Object.keys(restaurant).length > 0 ? true : false
-  const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 'auto',
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
-    boxShadow: 24,
-    p: 2
-  }
+	const initialValues = {
+		name: restaurant?.name ?? '',
+		city: restaurant?.city ?? '',
+		longitude: restaurant?.location?.coordinates[1] ?? '',
+		latitude: restaurant?.location?.coordinates[0] ?? '',
+		price: restaurant?.price ?? '',
+		textContent: restaurant?.textContent ?? '',
+		isVenue: restaurant?.isVenue ?? false
+	}
 
-  return (
-    <>
-      <Modal open={open} onClose={handleClose}>
-        <Box sx={style} style={{ paddingRight: '0px' }}>
-          <ImageList sx={{ width: 520, height: 450 }} cols={3} rowHeight={164}>
-            {restaurant?.imageContentUrl?.map((item, index) => (
-              <ImageListItem key={index} style={{ position: 'relative' }}>
-                <div
-                  style={{
-                    position: 'absolute',
-                    cursor: 'pointer',
-                    color: 'red',
-                    margin: '1px'
-                  }}
-                  onClick={() => {
-                    const arr = [...imgList]
-                    if (index > -1) {
-                      arr.push(restaurant.imageContentUrl[index])
-                      setImgList(arr)
-                      restaurant.imageContentUrl.splice(index, 1) // 2nd parameter means remove one item only
-                    }
-                    setIsUpdate(!isUpdate)
-                  }}
-                >
-                  <Icon icon='material-symbols:cancel' width='30' />
-                </div>
-                <img
-                  src={`${item}?w=164&h=164&fit=crop&auto=format`}
-                  srcSet={`${item}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-                  loading='lazy'
-                />
-              </ImageListItem>
-            ))}
-          </ImageList>
-          <div className='flex align-center justify-end p-4'>
-            <Formik
-              initialValues={initialValues}
-              onSubmit={(values) => {
-                values['imageContentUrl'] = restaurant.imageContentUrl
-                values['deletedImage'] = imgList
-                submitForm(
-                  values,
-                  fileInput.current.files ?? [],
-                  'restaurants/image',
-                  update
-                )
-              }}
-            >
-              {(formik) => (
-                <div>
-                  <Form>
-                    <fieldset className='grid grid-cols-2 gap-4'>
-                      <div className='flex align-center justify-start'>
-                        <label
-                          htmlFor='file-upload'
-                          className='custom-file-upload'
-                        ></label>
-                        <input
-                          id='file-upload'
-                          type='file'
-                          ref={fileInput}
-                          name='imageContentUrl'
-                          multiple
-                        />
-                      </div>
-                      <input
-                        type='submit'
-                        className='cursor-pointer py-2 px-10 hover:bg-gray-600 bg-green-50 text-black-50 hover:text-white-50 fonrt-bold uppercase rounded-lg'
-                        value='Edit now'
-                      />
-                    </fieldset>
-                  </Form>
-                </div>
-              )}
-            </Formik>
-          </div>
-        </Box>
-      </Modal>
-      <Formik
-        initialValues={initialValues}
-        onSubmit={(values) => {
-          submitForm(
-            values,
-            fileInput?.current.files ?? [],
-            'restaurants',
-            update
-          )
-        }}
-        enableReinitialize
-        validationSchema={Yup.object({
-          name: Yup.string().required('Required'),
-          city: Yup.string().required('Required'),
-          longitude: Yup.number().required('Required'),
-          latitude: Yup.number().required('Required'),
-          price: Yup.number().required('Required'),
-          isVenue: Yup.boolean()
-        })}
-      >
-        {(formik) => (
-          <div className='block p-6 rounded-lg shadow-lg bg-white w-3/4'>
-            <Form>
-              <fieldset className='grid grid-cols-2 gap-4'>
-                <legend>
-                  <h1 className='text-2xl mb-4'>General Restaurant Data</h1>
-                </legend>
-                <div className='form-group mb-6'>
-                  <TextInput
-                    label='Name'
-                    name='name'
-                    placeholder='Restaurant Name'
-                    type='text'
-                  />
-                  <SelectInput
-                    label='Group Location'
-                    name='city'
-                    placeholder='Barcelona ...'
-                    options={locations}
-                    value={formik.values.city}
-                  />
-                  <TextInput
-                    label='Coords Longitude'
-                    name='longitude'
-                    placeholder='ex : 2.154007'
-                    type='number'
-                  />
-                  <TextInput
-                    label='Coords Latitude'
-                    name='latitude'
-                    placeholder='ex : 41.390205'
-                    type='number'
-                  />
-                  <TextInput
-                    label='Average Menu Price'
-                    name='price'
-                    placeholder='ex : 35'
-                    type='number'
-                  />
-                </div>
-                <div className='form-group mb-6'>
-                  <CheckboxInput label='It is a venue' name='isVenue' />
-                  <TextAreaInput
-                    name='textContent'
-                    className='
+	const update = Object.keys(restaurant).length > 0 ? true : false
+	const style = {
+		position: 'absolute',
+		top: '50%',
+		left: '50%',
+		transform: 'translate(-50%, -50%)',
+		width: 'auto',
+		bgcolor: 'background.paper',
+		border: '2px solid #000',
+		boxShadow: 24,
+		p: 2
+	}
+
+	return (
+		<>
+			<Modal open={open} onClose={handleClose}>
+				<Box sx={style} style={{ paddingRight: '0px' }}>
+					<ImageList sx={{ width: 520, height: 450 }} cols={3} rowHeight={164}>
+						{restaurant?.imageContentUrl?.map((item, index) => (
+							<ImageListItem key={index} style={{ position: 'relative' }}>
+								<div
+									style={{
+										position: 'absolute',
+										cursor: 'pointer',
+										color: 'red',
+										margin: '1px'
+									}}
+									onClick={() => {
+										const arr = [...imgList]
+										if (index > -1) {
+											arr.push(restaurant.imageContentUrl[index])
+											setImgList(arr)
+											restaurant.imageContentUrl.splice(index, 1) // 2nd parameter means remove one item only
+										}
+										setIsUpdate(!isUpdate)
+									}}
+								>
+									<Icon icon="material-symbols:cancel" width="30" />
+								</div>
+								<img
+									src={`${item}?w=164&h=164&fit=crop&auto=format`}
+									srcSet={`${item}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+									loading="lazy"
+								/>
+							</ImageListItem>
+						))}
+					</ImageList>
+					<div className="flex align-center justify-end p-4">
+						<Formik
+							initialValues={initialValues}
+							onSubmit={(values) => {
+								values['imageContentUrl'] = restaurant.imageContentUrl
+								values['deletedImage'] = imgList
+								submitForm(
+									values,
+									fileInput.current.files ?? [],
+									'restaurants/image',
+									update
+								)
+							}}
+						>
+							{(formik) => (
+								<div>
+									<Form>
+										<fieldset className="grid grid-cols-2 gap-4">
+											<div className="flex align-center justify-start">
+												<label
+													htmlFor="file-upload"
+													className="custom-file-upload"
+												></label>
+												<input
+													id="file-upload"
+													type="file"
+													ref={fileInput}
+													name="imageContentUrl"
+													multiple
+												/>
+											</div>
+											<input
+												type="submit"
+												className="cursor-pointer py-2 px-10 hover:bg-gray-600 bg-green-50 text-black-50 hover:text-white-50 fonrt-bold uppercase rounded-lg"
+												value="Edit now"
+											/>
+										</fieldset>
+									</Form>
+								</div>
+							)}
+						</Formik>
+					</div>
+				</Box>
+			</Modal>
+			<Formik
+				initialValues={initialValues}
+				onSubmit={(values) => {
+					submitForm(
+						values,
+						fileInput?.current.files ?? [],
+						'restaurants',
+						update
+					)
+				}}
+				enableReinitialize
+				validationSchema={Yup.object({
+					name: Yup.string().required('Required'),
+					city: Yup.string().required('Required'),
+					longitude: Yup.number().required('Required'),
+					latitude: Yup.number().required('Required'),
+					price: Yup.number().required('Required'),
+					isVenue: Yup.boolean()
+				})}
+			>
+				{(formik) => (
+					<div className="block p-6 rounded-lg shadow-lg bg-white w-3/4">
+						<Form>
+							<fieldset className="grid grid-cols-2 gap-4">
+								<legend>
+									<h1 className="text-2xl mb-4">General Restaurant Data</h1>
+								</legend>
+								<div className="form-group mb-6">
+									<TextInput
+										label="Name"
+										name="name"
+										placeholder="Restaurant Name"
+										type="text"
+									/>
+									<SelectInput
+										label="Group Location"
+										name="city"
+										placeholder="Barcelona ..."
+										options={locations}
+										value={formik.values.city}
+									/>
+									<TextInput
+										label="Coords Longitude"
+										name="longitude"
+										placeholder="ex : 2.154007"
+										type="number"
+									/>
+									<TextInput
+										label="Coords Latitude"
+										name="latitude"
+										placeholder="ex : 41.390205"
+										type="number"
+									/>
+									<TextInput
+										label="Average Menu Price"
+										name="price"
+										placeholder="ex : 35"
+										type="number"
+									/>
+								</div>
+								<div className="form-group mb-6">
+									<CheckboxInput label="It is a venue" name="isVenue" />
+									<TextAreaInput
+										name="textContent"
+										className="
                      form-control
                      h-52
                      block
@@ -203,48 +203,48 @@ const RestaurantMasterForm = ({ submitForm, restaurant }) => {
                      ease-in-out
                      mt-7
                      focus:text-gray-700 focus:outline-none
-                   '
-                    placeholder='Write a description of the restaurant'
-                    type='text'
-                  />
-                  <div className='flex align-center justify-start'>
-                    <label htmlFor='file-upload' className='custom-file-upload'>
-                      <Icon icon='akar-icons:cloud-upload' width='40' />
-                    </label>
-                    <input
-                      id='file-upload'
-                      type='file'
-                      ref={fileInput}
-                      name='imageContentUrl'
-                      multiple
-                      disabled={update ? true : false}
-                    />
-                  </div>
-                </div>
-                <input
-                  type='submit'
-                  className='cursor-pointer py-2 px-10 hover:bg-gray-600 bg-green-50 text-black-50 hover:text-white-50 fonrt-bold uppercase rounded-lg'
-                  value={
-                    update ? 'Edit Restaurant Form' : 'Save new Restaurant'
-                  }
-                />
-                {restaurant?.name && (
-                  <div className='flex align-center justify-start'>
-                    <input
-                      onClick={handleOpen}
-                      type='button'
-                      className='cursor-pointer py-2 px-10 hover:bg-gray-600 bg-green-50 text-black-50 hover:text-white-50 fonrt-bold uppercase rounded-lg'
-                      value='Show images'
-                    />
-                  </div>
-                )}
-              </fieldset>
-            </Form>
-          </div>
-        )}
-      </Formik>
-    </>
-  )
+                   "
+										placeholder="Write a description of the restaurant"
+										type="text"
+									/>
+									<div className="flex align-center justify-start">
+										<label htmlFor="file-upload" className="custom-file-upload">
+											<Icon icon="akar-icons:cloud-upload" width="40" />
+										</label>
+										<input
+											id="file-upload"
+											type="file"
+											ref={fileInput}
+											name="imageContentUrl"
+											multiple
+											disabled={update ? true : false}
+										/>
+									</div>
+								</div>
+								<input
+									type="submit"
+									className="cursor-pointer py-2 px-10 hover:bg-gray-600 bg-green-50 text-black-50 hover:text-white-50 fonrt-bold uppercase rounded-lg"
+									value={
+										update ? 'Edit Restaurant Form' : 'Save new Restaurant'
+									}
+								/>
+								{restaurant?.name && (
+									<div className="flex align-center justify-start">
+										<input
+											onClick={handleOpen}
+											type="button"
+											className="cursor-pointer py-2 px-10 hover:bg-gray-600 bg-green-50 text-black-50 hover:text-white-50 fonrt-bold uppercase rounded-lg"
+											value="Show images"
+										/>
+									</div>
+								)}
+							</fieldset>
+						</Form>
+					</div>
+				)}
+			</Formik>
+		</>
+	)
 }
 
 export default RestaurantMasterForm
