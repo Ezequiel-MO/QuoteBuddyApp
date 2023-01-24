72d53c1780ab5372f0e4b9a65e379702d7dfac8f
Ezequiel <oliverm316@gmail.com>
2023-01-24 17:17:13 +0100

    fixes to see all vendors from multiple cities when added to project

--- src/screens/events/EventList/EventList.jsx
+++ src/screens/events/EventList/EventList.jsx
@@ -2,97 +2,98 @@ import { useState, useEffect } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom'
 import EventListItem from './EventListItem'
 import {
-  CityFilter,
-  PriceFilter,
-  Spinner,
-  TableHeaders,
-  SearchInput
+	CityFilter,
+	PriceFilter,
+	Spinner,
+	TableHeaders,
+	SearchInput
 } from '../../../ui'
 import 'react-toastify/dist/ReactToastify.css'
 import { useCurrentProject, useGetEvents } from '../../../hooks'
 
 const EventList = () => {
-  const navigate = useNavigate()
-  const location = useLocation()
-  const [event] = useState({})
-  const { currentProject } = useCurrentProject()
-  const { groupLocation } = currentProject
-  const [city, setCity] = useState(groupLocation || '')
-  const [searchItem, setSearchItem] = useState('')
-  const [price, setPrice] = useState(0)
-  const { events, setEvents, isLoading } = useGetEvents(city, price)
-  const currentProjectIsLive = Object.keys(currentProject).length !== 0
-  const [foundEvents, setFoundEvents] = useState([])
+	const navigate = useNavigate()
+	const location = useLocation()
+	const [event] = useState({})
+	const { currentProject } = useCurrentProject()
+	const { groupLocation } = currentProject
+	const [city, setCity] = useState(groupLocation || '')
+	const [searchItem, setSearchItem] = useState('')
+	const [price, setPrice] = useState(0)
+	const { events, setEvents, isLoading } = useGetEvents(city, price)
+	const currentProjectIsLive = Object.keys(currentProject).length !== 0
+	const [foundEvents, setFoundEvents] = useState([])
 
-  useEffect(() => {
-    setFoundEvents(events)
-  }, [events])
+	useEffect(() => {
+		setFoundEvents(events)
+	}, [events])
 
-  const addEventToProject = (event) => {
-    navigate(`/app/project/schedule/${event._id}`, {
-      state: {
-        event,
-        dayOfEvent: location.state.dayOfEvent,
-        timeOfEvent: location.state.timeOfEvent
-      }
-    })
-  }
+	const addEventToProject = (event) => {
+		navigate(`/app/project/schedule/${event._id}`, {
+			state: {
+				event,
+				dayOfEvent: location.state.dayOfEvent,
+				timeOfEvent: location.state.timeOfEvent
+			}
+		})
+	}
 
-  const filterList = (e) => {
-    setSearchItem(e.target.value)
-    const result = events.filter((data) =>
-      data.name.toLowerCase().includes(e.target.value.toLowerCase())
-    )
-    setFoundEvents(result)
-    if (searchItem === '') {
-      setFoundEvents(events)
-    }
-  }
+	const filterList = (e) => {
+		setSearchItem(e.target.value)
+		const result = events.filter((data) =>
+			data.name.toLowerCase().includes(e.target.value.toLowerCase())
+		)
+		setFoundEvents(result)
+		if (searchItem === '') {
+			setFoundEvents(events)
+		}
+	}
 
-  const eventList = foundEvents?.map((event) => (
-    <EventListItem
-      key={event._id}
-      event={event}
-      events={events}
-      setEvents={setEvents}
-      addEventToProject={addEventToProject}
-      canBeAddedToProject={location.state}
-    />
-  ))
+	const eventList = foundEvents?.map((event) => (
+		<EventListItem
+			key={event._id}
+			event={event}
+			events={events}
+			setEvents={setEvents}
+			addEventToProject={addEventToProject}
+			canBeAddedToProject={location.state}
+		/>
+	))
 
-  return (
-    <>
-      <div className='flex flex-col sm:flex-row sm:items-end items-start sm:space-x-6 mb-4 mr-8 ml-8'>
-        <div className='flex flex-col w-full'>
-          <h1 className='text-2xl'>Event List</h1>
-          <div className='flex flex-row justify-start items-center'>
-            <div>
-              {currentProjectIsLive ? null : <CityFilter setCity={setCity} />}
-              <PriceFilter setPrice={setPrice} />
-            </div>
-            <button
-              onClick={() => navigate('/app/event/specs', { state: { event } })}
-              className='mx-5 focus:scale-110 hover:animate-pulse bg-transparent hover:bg-orange-50 text-white-100 uppercase font-semibold hover:text-black-50 py-2 px-4 border border-orange-50 hover:border-transparent rounded'
-            >
-              Create New Event
-            </button>
-            <SearchInput searchItem={searchItem} filterList={filterList} />
-          </div>
-        </div>
-      </div>
-      <hr />
-      <div className='flex-1 m-4 flex-col'>
-        {isLoading ? (
-          <Spinner />
-        ) : (
-          <table className='w-full p-5'>
-            <TableHeaders headers='event' />
-            {eventList}
-          </table>
-        )}
-      </div>
-    </>
-  )
+	return (
+		<>
+			<div className="flex flex-col sm:flex-row sm:items-end items-start sm:space-x-6 mb-4 mr-8 ml-8">
+				<div className="flex flex-col w-full">
+					<h1 className="text-2xl">Event List</h1>
+					<div className="flex flex-row justify-start items-center">
+						<div>
+							{/*  {currentProjectIsLive ? null : <CityFilter setCity={setCity} />} */}
+							<CityFilter setCity={setCity} />
+							<PriceFilter setPrice={setPrice} />
+						</div>
+						<button
+							onClick={() => navigate('/app/event/specs', { state: { event } })}
+							className="mx-5 focus:scale-110 hover:animate-pulse bg-transparent hover:bg-orange-50 text-white-100 uppercase font-semibold hover:text-black-50 py-2 px-4 border border-orange-50 hover:border-transparent rounded"
+						>
+							Create New Event
+						</button>
+						<SearchInput searchItem={searchItem} filterList={filterList} />
+					</div>
+				</div>
+			</div>
+			<hr />
+			<div className="flex-1 m-4 flex-col">
+				{isLoading ? (
+					<Spinner />
+				) : (
+					<table className="w-full p-5">
+						<TableHeaders headers="event" />
+						{eventList}
+					</table>
+				)}
+			</div>
+		</>
+	)
 }
 
 export default EventList
--- src/screens/restaurants/RestaurantList/RestaurantList.jsx
+++ src/screens/restaurants/RestaurantList/RestaurantList.jsx
@@ -4,109 +4,110 @@ import RestaurantListItem from './RestaurantListItem'
 import { useCurrentProject, useGetRestaurants } from '../../../hooks'
 import { TableHeaders } from '../../../ui'
 import {
-  CityFilter,
-  PriceFilter,
-  RestaurantVenueFilter,
-  SearchInput,
-  Spinner
+	CityFilter,
+	PriceFilter,
+	RestaurantVenueFilter,
+	SearchInput,
+	Spinner
 } from '../../../ui'
 
 const RestaurantList = () => {
-  const navigate = useNavigate()
-  const location = useLocation()
-  const [restaurant] = useState({})
-  const [price, setPrice] = useState(900)
-  const [venueOrRestaurant, setVenueOrRestaurant] = useState('all')
-  const [searchItem, setSearchItem] = useState('')
-  const { currentProject } = useCurrentProject()
-  const { groupLocation } = currentProject
-  const [city, setCity] = useState(groupLocation || '')
-  const { restaurants, setRestaurants, isLoading } = useGetRestaurants(
-    city,
-    price,
-    venueOrRestaurant
-  )
-  const [foundRestaurants, setFoundRestaurants] = useState([])
+	const navigate = useNavigate()
+	const location = useLocation()
+	const [restaurant] = useState({})
+	const [price, setPrice] = useState(900)
+	const [venueOrRestaurant, setVenueOrRestaurant] = useState('all')
+	const [searchItem, setSearchItem] = useState('')
+	const { currentProject } = useCurrentProject()
+	const { groupLocation } = currentProject
+	const [city, setCity] = useState(groupLocation || '')
+	const { restaurants, setRestaurants, isLoading } = useGetRestaurants(
+		city,
+		price,
+		venueOrRestaurant
+	)
+	const [foundRestaurants, setFoundRestaurants] = useState([])
 
-  useEffect(() => {
-    setFoundRestaurants(restaurants)
-  }, [restaurants])
+	useEffect(() => {
+		setFoundRestaurants(restaurants)
+	}, [restaurants])
 
-  const currentProjectIsLive = Object.keys(currentProject).length !== 0
+	const currentProjectIsLive = Object.keys(currentProject).length !== 0
 
-  const addRestaurantToProject = (restaurant) => {
-    navigate(`/app/project/schedule/${restaurant._id}`, {
-      state: {
-        event: restaurant,
-        dayOfEvent: location.state.dayOfEvent,
-        timeOfEvent: location.state.timeOfEvent
-      }
-    })
-  }
+	const addRestaurantToProject = (restaurant) => {
+		navigate(`/app/project/schedule/${restaurant._id}`, {
+			state: {
+				event: restaurant,
+				dayOfEvent: location.state.dayOfEvent,
+				timeOfEvent: location.state.timeOfEvent
+			}
+		})
+	}
 
-  const filterList = (e) => {
-    setSearchItem(e.target.value)
-    const result = restaurants.filter((data) =>
-      data.name.toLowerCase().includes(e.target.value.toLowerCase())
-    )
-    setFoundRestaurants(result)
-    if (searchItem === '') {
-      setFoundRestaurants(restaurants)
-    }
-  }
+	const filterList = (e) => {
+		setSearchItem(e.target.value)
+		const result = restaurants.filter((data) =>
+			data.name.toLowerCase().includes(e.target.value.toLowerCase())
+		)
+		setFoundRestaurants(result)
+		if (searchItem === '') {
+			setFoundRestaurants(restaurants)
+		}
+	}
 
-  const restaurantList = foundRestaurants?.map((restaurant) => (
-    <RestaurantListItem
-      key={restaurant._id}
-      restaurant={restaurant}
-      restaurants={restaurants}
-      setRestaurants={setRestaurants}
-      addRestaurantToProject={addRestaurantToProject}
-      canBeAddedToProject={location.state}
-    />
-  ))
+	const restaurantList = foundRestaurants?.map((restaurant) => (
+		<RestaurantListItem
+			key={restaurant._id}
+			restaurant={restaurant}
+			restaurants={restaurants}
+			setRestaurants={setRestaurants}
+			addRestaurantToProject={addRestaurantToProject}
+			canBeAddedToProject={location.state}
+		/>
+	))
 
-  return (
-    <>
-      <div className='flex flex-col sm:flex-row sm:items-end items-start sm:space-x-6 mb-4 mr-8 ml-8'>
-        <div className='flex flex-col w-full'>
-          <h1 className='text-2xl'>Restaurant List</h1>
-          <div className='flex flex-row justify-start items-center'>
-            <div>
-              {currentProjectIsLive ? null : (
+	return (
+		<>
+			<div className="flex flex-col sm:flex-row sm:items-end items-start sm:space-x-6 mb-4 mr-8 ml-8">
+				<div className="flex flex-col w-full">
+					<h1 className="text-2xl">Restaurant List</h1>
+					<div className="flex flex-row justify-start items-center">
+						<div>
+							{/*  {currentProjectIsLive ? null : (
                 <CityFilter setCity={setCity} city={city} />
-              )}
-              <PriceFilter setPrice={setPrice} price={price} />
-              <RestaurantVenueFilter
-                setVenueOrRestaurant={setVenueOrRestaurant}
-                venueOrRestaurant={venueOrRestaurant}
-              />
-            </div>
+              )} */}
+							<CityFilter setCity={setCity} city={city} />
+							<PriceFilter setPrice={setPrice} price={price} />
+							<RestaurantVenueFilter
+								setVenueOrRestaurant={setVenueOrRestaurant}
+								venueOrRestaurant={venueOrRestaurant}
+							/>
+						</div>
 
-            <button
-              onClick={() =>
-                navigate('/app/restaurant/specs', { state: { restaurant } })
-              }
-              className='mx-5 focus:scale-110 hover:animate-pulse bg-transparent hover:bg-orange-50 text-white-100 uppercase font-semibold hover:text-black-50 py-2 px-4 border border-orange-50 hover:border-transparent rounded'
-            >
-              Create New Restaurant
-            </button>
-            <SearchInput searchItem={searchItem} filterList={filterList} />
-          </div>
-        </div>
-      </div>
+						<button
+							onClick={() =>
+								navigate('/app/restaurant/specs', { state: { restaurant } })
+							}
+							className="mx-5 focus:scale-110 hover:animate-pulse bg-transparent hover:bg-orange-50 text-white-100 uppercase font-semibold hover:text-black-50 py-2 px-4 border border-orange-50 hover:border-transparent rounded"
+						>
+							Create New Restaurant
+						</button>
+						<SearchInput searchItem={searchItem} filterList={filterList} />
+					</div>
+				</div>
+			</div>
 
-      <hr />
-      {isLoading ? (
-        <Spinner />
-      ) : (
-        <table className='w-full p-5'>
-          <TableHeaders headers='restaurant' />
-          {restaurantList}
-        </table>
-      )}
-    </>
-  )
+			<hr />
+			{isLoading ? (
+				<Spinner />
+			) : (
+				<table className="w-full p-5">
+					<TableHeaders headers="restaurant" />
+					{restaurantList}
+				</table>
+			)}
+		</>
+	)
 }
 
 export default RestaurantList
