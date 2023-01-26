0d3d01dc2c0c69a373e4c239a06c13c963a86eda
edgardo90 <edgardo9000@gmail.com>
2023-01-26 01:07:50 -0300

    basic pagination implemented for accManagers y hotels

--- src/screens/settings/SettingsPage.jsx
+++ src/screens/settings/SettingsPage.jsx
@@ -1,10 +1,6 @@
 import { Toggle } from '../../ui'
-import {useState} from "react"
-
 
 const SettingsPage = () => {
-  const [data , setData] = useState(false)
-
   return (
     <div>
       <Toggle />
