cc70fa8aed84b75923c233bd1ebeb38c2e9fac45
Ezequiel <oliverm316@gmail.com>
2023-01-26 10:51:40 +0100

    conflict resolved

--- src/screens/settings/SettingsPage.jsx
+++ src/screens/settings/SettingsPage.jsx
@@ -1,52 +1,50 @@
-import {useState , useRef} from "react"
+import { useState, useRef } from 'react'
 import { toast } from 'react-toastify'
 import baseAPI from '../../axios/axiosConfig'
 import { errorToastOptions, toastOptions } from '../../helper/toast'
 import { Toggle } from '../../ui'
-import SettingsForm  from "./SettingsForm"
-
+import SettingsForm from './SettingsForm'
 
 const SettingsPage = () => {
-  const fileInput = useRef()
+	const fileInput = useRef()
 
-  const [check , setCheck] = useState(false)
-  
-  const [data , setData] = useState({
-    name:"",
-    colorPalette:[],
-    font:"",
-  })
+	const [check, setCheck] = useState(false)
 
+	const [data, setData] = useState({
+		name: '',
+		colorPalette: [],
+		font: ''
+	})
 
-
-  const submitForm = async (event) => {
-		event.preventDefault();
+	const submitForm = async (event) => {
+		event.preventDefault()
 		try {
-				// let res = await baseAPI.post('v1/company_features', data)
-				console.log(fileInput.current)
-        console.log(data)
-				toast.success('User Created', toastOptions)
+			// let res = await baseAPI.post('v1/company_features', data)
+			console.log(fileInput.current)
+			console.log(data)
+			toast.success('Company Features Created', toastOptions)
 		} catch (err) {
-			console.log(err.response);
-			toast.error(`Error Creating/Updating User, ${err.response.data.msg}`,
-				errorToastOptions)
+			console.log(err.response)
+			toast.error(
+				`Error Creating/Updating Company Features, ${err.response.data.msg}`,
+				errorToastOptions
+			)
 		}
 	}
 
-  return (
-    <div>
-      <Toggle check={check} setCheck={setCheck} />
-      {
-       check && 
-        <SettingsForm
-         data={data}
-         setData={setData}
-         fileInput={fileInput}
-         handleSubmit={submitForm}
-        />
-      }
-    </div>
-  )
+	return (
+		<div>
+			<Toggle check={check} setCheck={setCheck} />
+			{check && (
+				<SettingsForm
+					data={data}
+					setData={setData}
+					fileInput={fileInput}
+					handleSubmit={submitForm}
+				/>
+			)}
+		</div>
+	)
 }
 
 export default SettingsPage
