0cb46b9d37c39e5774bade9801d515612a39f2fb
Ezequiel <oliverm316@gmail.com>
2023-01-26 12:41:31 +0100

    invoice list restyled

--- src/screens/invoices/InvoiceList/InvoiceListItem.jsx
+++ src/screens/invoices/InvoiceList/InvoiceListItem.jsx
@@ -1,14 +1,14 @@
 import { useNavigate } from 'react-router-dom'
 import accounting from 'accounting'
 import { useCurrentInvoice } from '../../../hooks/useCurrentInvoice'
-import {useAuth} from '../../../hooks'
-import {ButtonDeleted} from "../../../components/atoms"
+import { useAuth } from '../../../hooks'
+import { ButtonDeleted } from '../../../components/atoms'
 
 const InvoiceListItem = ({ invoice, invoices, setInvoices }) => {
 	const navigate = useNavigate()
 	const { setInvoice } = useCurrentInvoice()
 
-	const {auth} = useAuth()
+	const { auth } = useAuth()
 
 	const handleClick = () => {
 		invoice.postingStatus = 'review'
@@ -16,30 +16,58 @@ const InvoiceListItem = ({ invoice, invoices, setInvoices }) => {
 		navigate(`/app/invoice/specs/${invoice._id}`)
 	}
 
+	const shortenDate = (date) => {
+		const dateArray = date.split(' ')
+		const day = dateArray[1].slice(0, -3)
+		const month = dateArray[0]
+		const year = dateArray[2]
+		const months = {
+			January: '01',
+			February: '02',
+			March: '03',
+			April: '04',
+			May: '05',
+			June: '06',
+			July: '07',
+			August: '08',
+			September: '09',
+			October: '10',
+			November: '11',
+			December: '12'
+		}
+		//check if `${day}/${months[month]}/${year}` contains the substring 'undefined', and return 'Invalid Date' if it does
+		if (`${day}/${months[month]}/${year}`.includes('undefined' || 'NaN')) {
+			return 'Inv. Date'
+		}
+		return `${day}/${months[month]}/${year}`
+	}
+
 	return (
 		<tbody>
 			<tr className="mb-2 p-1 bg-gray-900 hover:bg-green-100 hover:text-black-50 rounded-md text-white-50">
 				<td
 					onClick={() => handleClick(invoice._id)}
-					className="hover:text-blue-600 hover:underline cursor-pointer"
+					className="hover:text-blue-600 hover:underline cursor-pointer border-r-[1px]"
+					style={{ width: '80px' }}
 				>{`${invoice.invoiceNumber} `}</td>
-				<td>{invoice.date}</td>
-				<td>{invoice.client}</td>
-				<td>{invoice.company}</td>
-				<td>{invoice.reference}</td>
-				<td>
+				<td className="truncate border-r-[1px] px-1">
+					{shortenDate(invoice.date)}
+				</td>
+				<td className="truncate border-r-[1px]">{invoice.client}</td>
+				<td className="truncate border-r-[1px]">{invoice.company}</td>
+				<td className="truncate border-r-[1px]">{invoice.reference}</td>
+				<td style={{ width: '300px' }}>
 					{accounting.formatMoney(invoice.lineAmount, `${invoice.currency} `)}
 				</td>
 				<td className="cursor-pointer">
-					{
-						auth.role === "admin" &&
+					{auth.role === 'admin' && (
 						<ButtonDeleted
-						endpoint={'invoices'}
-						ID={invoice._id}
-						setter={setInvoices}
-						items={invoices}
+							endpoint={'invoices'}
+							ID={invoice._id}
+							setter={setInvoices}
+							items={invoices}
 						/>
-					}
+					)}
 				</td>
 			</tr>
 		</tbody>
