fcf3d7527b864ca42b2b719c07fd64a047d1cec3
Ezequiel <oliverm316@gmail.com>
2023-02-06 10:00:40 +0100

    minor fix

--- src/helper/dashboardData.js
+++ src/helper/dashboardData.js
@@ -1,16 +1,16 @@
 export const dashboardData = [
-  { title: 'Hotels', route: 'hotel', icon: 'bxs:hotel' },
-  { title: 'Restaurants', route: 'restaurant', icon: 'bx:restaurant' },
-  { title: 'Events', route: 'event', icon: 'ic:baseline-event-available' },
-  { title: 'Transfers', route: 'transfer', icon: 'cil:bus-alt' },
-  { title: 'Clients', route: 'client', icon: 'mdi:handshake-outline' },
-  { title: 'Locations', route: 'location', icon: 'akar-icons:location' },
-  { title: 'Account Managers', route: 'accManager', icon: 'akar-icons:person' },
-  { title: 'Countries', route: 'country', icon: 'gis:search-country' },
-  {
-    title: 'companies',
-    route: 'company',
-    icon: 'mdi:company'
-  },
-  { title: "Users" , route:"user" , icon: 'akar-icons:person'},
+	{ title: 'Hotels', route: 'hotel', icon: 'bxs:hotel' },
+	{ title: 'Restaurants', route: 'restaurant', icon: 'bx:restaurant' },
+	{ title: 'Events', route: 'event', icon: 'ic:baseline-event-available' },
+	{ title: 'Transfers', route: 'transfer', icon: 'cil:bus-alt' },
+	{ title: 'Clients', route: 'client', icon: 'mdi:handshake-outline' },
+	{ title: 'Locations', route: 'location', icon: 'akar-icons:location' },
+	{ title: 'Account Managers', route: 'accManager', icon: 'akar-icons:person' },
+	{ title: 'Countries', route: 'country', icon: 'gis:search-country' },
+	{
+		title: 'Companies',
+		route: 'company',
+		icon: 'mdi:company'
+	},
+	{ title: 'Users', route: 'user', icon: 'akar-icons:person' }
 ]
