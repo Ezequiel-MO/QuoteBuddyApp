d4f3d442caa1b6a2c8d416a42d48ac906f4ce47a
Ezequiel <oliverm316@gmail.com>
2023-03-13 17:52:38 +0100

    DRAG AND DROP- WIP

--- src/screens/projects/render/schedule/render/DayEvents.jsx
+++ src/screens/projects/render/schedule/render/DayEvents.jsx
@@ -1,27 +1,43 @@
 import { Icon } from '@iconify/react'
-import styles from "./render.module.css"
+import styles from './render.module.css'
 
 export const DayEvents = ({ day, event, handleDeleteEvent, dayIndex }) => {
-    
-    return (
-        <>
-            {
-                day[event].map((el) => {
-                    return (
-                        <div key={el._id} className={styles.list}>
-                            <p style={{ fontSize: "16px" }}>
-                                {el.name}
-                            </p>
-                            <span
-                                className={styles.deleted}
-                                onClick={() => handleDeleteEvent(dayIndex, event, el._id)}
-                            >
-                                <Icon icon="lucide:delete" color="#ea5933" />
-                            </span>
-                        </div>
-                    )
-                })
-            }
-        </>
-    )
+	const handleDragStart = (e, el, index, event) => {
+		console.log('onDragStart:', e, 'elemento arrastrado', el)
+		console.log('dayOfEvent', index, 'timeOfEvent', event)
+	}
+
+	const handleDrop = (e, el, index, event) => {
+		console.log('onDrop:', e, 'elemento dejado', el)
+		console.log('dayOfEvent', index, 'timeOfEvent', event)
+	}
+
+	const handleDragOver = (e) => {
+		e.preventDefault()
+	}
+
+	return (
+		<>
+			{day[event].map((el) => {
+				return (
+					<div
+						key={el._id}
+						className={styles.list}
+						draggable
+						onDragStart={(e) => handleDragStart(e, el, dayIndex, event)}
+						onDrop={(e) => handleDrop(e, el, dayIndex, event)}
+						onDragOver={(e) => handleDragOver(e)}
+					>
+						<p style={{ fontSize: '16px' }}>{el.name}</p>
+						<span
+							className={styles.deleted}
+							onClick={() => handleDeleteEvent(dayIndex, event, el._id)}
+						>
+							<Icon icon="lucide:delete" color="#ea5933" />
+						</span>
+					</div>
+				)
+			})}
+		</>
+	)
 }
