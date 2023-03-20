b2b10a7dabc232b533111d437804591026c0b592
Ezequiel <oliverm316@gmail.com>
2023-03-20 13:52:31 +0100

    minor fix to try to handle commonjs dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import commonjs from 'vite-plugin-commonjs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    chunkSizeWarningLimit: 1600
-  }
+	plugins: [react(), commonjs()],
+	build: {
+		chunkSizeWarningLimit: 1600
+	}
 })
