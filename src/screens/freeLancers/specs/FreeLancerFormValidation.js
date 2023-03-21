554bb3d3aaa78754a4af16cd9acd30b9cc14c51c
edgardo90 <edgardo9000@gmail.com>
2023-03-21 04:16:55 -0300

    #22_ESTRUCTURA_CARPETAS

---
+++ src/screens/freeLancers/specs/FreeLancerFormValidation.js
@@ -0,0 +1,15 @@
+import * as yup from 'yup'
+
+export const getValidationSchema  = yup.object().shape({
+    firstName: yup.string().required("Required"),
+    familyName: yup.string().required("Required"),
+    email: yup.string().email().required("Required"),
+    phone: yup.string().required("Required"),
+    halfDayRate: yup.number().min(1,"Required"),
+    fullDayRate: yup.number().min(1,"Required"),
+    languageSupplement: yup.number().min(1,"Required"),
+    weekendHDRate: yup.number().min(1,"Required"),
+    weekendFDRate: yup.number().min(1,"Required"),
+    type: yup.string().required("Required"),
+    city: yup.string().required("Required"),
+  });
