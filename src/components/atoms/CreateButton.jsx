7a3da536e0ab414e82634bdda79ee69d418c48ce
Ezequiel <oliverm316@gmail.com>
2023-04-06 10:30:55 +0200

    CreateButton created

---
+++ src/components/atoms/CreateButton.jsx
@@ -0,0 +1,10 @@
+export const CreateButton = ({ title, handleClick }) => {
+	return (
+		<button
+			onClick={handleClick}
+			className="mx-5 focus:scale-110 hover:animate-pulse bg-transparent hover:bg-orange-50 text-white-100 uppercase font-semibold hover:text-black-50 py-2 px-4 border border-orange-50 hover:border-transparent rounded"
+		>
+			Create New {title}
+		</button>
+	)
+}
