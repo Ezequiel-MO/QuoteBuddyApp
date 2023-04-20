d5fc44aace85a3018af8135cadb4b5bc7889a84d
Ezequiel <oliverm316@gmail.com>
2023-04-20 10:25:08 +0200

    project list restyled

--- src/screens/projects/list/ProjectListItem.jsx
+++ src/screens/projects/list/ProjectListItem.jsx
@@ -18,7 +18,7 @@ export const ProjectListItem = ({
 		<tbody>
 			<tr className="mb-2 p-1 bg-gray-900 hover:bg-green-100 hover:text-black-50 rounded-md text-white-50">
 				<td
-					className="cursor-pointer"
+					className="cursor-pointer w-12 text-center"
 					onClick={() => handleRecycleProject(project._id)}
 				>
 					<Icon icon="ic:round-system-update-alt" color="#ea5933" />
@@ -29,18 +29,18 @@ export const ProjectListItem = ({
 							state: { project }
 						})
 					}
-					className="hover:text-blue-600 hover:underline cursor-pointer"
+					className="hover:text-blue-600 hover:underline cursor-pointer truncate w-24"
 				>
 					{project.code}
 				</td>
-				<td>{project.groupLocation}</td>
-				<td>{project.groupName}</td>
-				<td>{project.nrPax}</td>
-				<td>{project.arrivalDay}</td>
-				<td>{project.departureDay}</td>
-				<td>{project.status}</td>
-				<td>{formatMoney(project.estimate)}</td>
-				<td className="cursor-pointer">
+				<td className="truncate w-32">{project.groupLocation}</td>
+				<td className="truncate w-32">{project.groupName}</td>
+				<td className="truncate w-12">{project.nrPax}</td>
+				<td className="truncate w-32">{project.arrivalDay}</td>
+				<td className="truncate w-32">{project.departureDay}</td>
+				<td className="truncate w-24">{project.status}</td>
+				<td className="truncate w-24">{formatMoney(project.estimate)}</td>
+				<td className="cursor-pointer w-12 text-center">
 					{auth.role === 'admin' && (
 						<ButtonDeleted
 							endpoint={'projects'}
