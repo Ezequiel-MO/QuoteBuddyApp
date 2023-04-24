d1231c5f0d007869f185690a56ea6f65f5c34538
Ezequiel <oliverm316@gmail.com>
2023-04-24 13:29:41 +0200

    minor fix

--- src/screens/projects/render/hotel/hotelModal/HotelModal.jsx
+++ src/screens/projects/render/hotel/hotelModal/HotelModal.jsx
@@ -1,136 +1,131 @@
-import { useState } from "react"
+import { useState } from 'react'
 import { Icon } from '@iconify/react'
-import { ModalComponent } from "../../../../../components/atoms/Modal"
-import { TableModalHotel } from "./TableModalHotel"
-import { ImagesModalHotel } from "./ImagesModalHotel"
+import { ModalComponent } from '../../../../../components/atoms/Modal'
+import { TableModalHotel } from './TableModalHotel'
+import { ImagesModalHotel } from './ImagesModalHotel'
 import { RichTextEditor } from '../../../../../ui'
 import { useCurrentProject } from '../../../../../hooks'
-import { validateUpdate } from "./helperHotelModal"
-import Swal from "sweetalert2"
-import withReactContent from "sweetalert2-react-content"
+import { validateUpdate } from './helperHotelModal'
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
 import styles from '../../DayEvents.module.css'
 
-export const HotelModal = ({ open, setOpen, hotel, index }) => {
+export const HotelModal = ({ open, setOpen, hotel }) => {
+	if (!hotel) {
+		return null
+	}
 
-    if (!hotel) {
-        return null
-    }
+	const mySwal = withReactContent(Swal)
 
-    const mySwal = withReactContent(Swal)
+	const { editModalHotel } = useCurrentProject()
+	const [textContent, setTextContent] = useState()
+	const [data, setData] = useState({})
+	const [isChecked, setIsChecked] = useState()
 
-    const { editModalHotel } = useCurrentProject()
-    const [textContent, setTextContent] = useState()
-    const [data, setData] = useState({})
-    const [isChecked, setIsChecked] = useState()
+	const styleModal = {
+		position: 'absolute',
+		top: '50%',
+		left: '50%',
+		transform: 'translate(-50%, -50%)',
+		width: '80%',
+		height: '90%',
+		bgcolor: 'background.paper',
+		border: '2px solid #000',
+		boxShadow: 24,
+		p: 2
+	}
 
-    const styleModal = {
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        width: '80%',
-        height: "90%",
-        bgcolor: 'background.paper',
-        border: '2px solid #000',
-        boxShadow: 24,
-        p: 2
-    }
+	const handleConfirm = () => {
+		mySwal
+			.fire({
+				title: 'Do you want to modify the data?',
+				icon: 'question',
+				showCancelButton: true,
+				confirmButtonText: 'yes',
+				cancelButtonText: `Cancel`,
+				customClass: { container: 'custom-container' }
+			})
+			.then((res) => {
+				if (res.isConfirmed) {
+					editModalHotel({
+						pricesEdit: data,
+						id: hotel._id
+					})
+					mySwal.fire({
+						title: 'Succes',
+						icon: 'success',
+						confirmButtonText: 'continue',
+						customClass: { container: 'custom-container' }
+					})
+					setOpen(false)
+				}
+			})
+	}
 
+	const handleClose = () => {
+		const validateIsChecked = validateUpdate(isChecked)
+		if (validateIsChecked) {
+			mySwal
+				.fire({
+					title: 'There is modified data',
+					text: 'Are you sure you want to exit? Your data will be lost',
+					icon: 'warning',
+					showCancelButton: true,
+					confirmButtonText: 'yes',
+					cancelButtonText: `Cancel`,
+					customClass: { container: 'custom-container' }
+				})
+				.then((res) => {
+					if (res.isConfirmed) {
+						setOpen(false)
+					}
+				})
+		} else {
+			setOpen(false)
+		}
+	}
 
-    const handleConfirm = () => {
-        mySwal.fire({
-            title: "Do you want to modify the data?",
-            icon: "question",
-            showCancelButton: true,
-            confirmButtonText: "yes",
-            cancelButtonText: `Cancell`,
-            customClass: { container: "custom-container" }
-        }).then((res) => {
-            if (res.isConfirmed) {
-                editModalHotel({
-                    pricesEdit: data,
-                    id: hotel._id,
-                })
-                mySwal.fire({
-                    title: "Succes",
-                    icon: "success",
-                    confirmButtonText: "continue",
-                    customClass: { container: "custom-container" }
-                })
-                setOpen(false)
-            }
-        })
-    }
+	const update = Object.keys(hotel).length > 0
 
-    const handleClose = () => {
-        const validateIsChecked = validateUpdate(isChecked)
-        if(validateIsChecked){
-            mySwal.fire({
-                title: "There are modified data",
-                text:"Are you sure you want to exit? There are modified data",
-                icon: "warning",
-                showCancelButton: true,
-                confirmButtonText: "yes",
-                cancelButtonText: `Cancell`,
-                customClass: { container: "custom-container" }
-            }).then((res)=>{
-                if(res.isConfirmed){
-                    setOpen(false)
-                }
-            })
-        }else{
-            setOpen(false)
-        }
-    }
+	return (
+		<div>
+			<ModalComponent open={open} setOpen={setOpen} styleModal={styleModal}>
+				<button className={styles.buttonCancel} onClick={() => handleClose()}>
+					<Icon icon="material-symbols:cancel" width="30" />
+				</button>
+				<div className="container w-3/4 flex flex-col bord">
+					<TableModalHotel
+						hotel={hotel}
+						data={data}
+						setData={setData}
+						isChecked={isChecked}
+						setIsChecked={setIsChecked}
+					/>
 
+					<div style={{ marginTop: '10px' }}>
+						<RichTextEditor
+							style={{}}
+							setTextContent={setTextContent}
+							textContent={textContent}
+							screen={hotel}
+							update={update}
+						/>
+					</div>
 
-    const update = Object.keys(hotel).length > 0
-
-    return (
-        <div>
-            <ModalComponent open={open} setOpen={setOpen} styleModal={styleModal}  >
-                <button
-                    className={styles.buttonCancel}
-                    onClick={() => handleClose()}
-                >
-                    <Icon icon="material-symbols:cancel" width="30" />
-                </button>
-                <div className="container w-3/4 flex flex-col bord">
-
-                    <TableModalHotel
-                        hotel={hotel}
-                        data={data}
-                        setData={setData}
-                        isChecked={isChecked}
-                        setIsChecked={setIsChecked}
-                    />
-
-                    <div style={{ marginTop: "10px" }}>
-                        <RichTextEditor
-                            style={{}}
-                            setTextContent={setTextContent}
-                            textContent={textContent}
-                            screen={hotel}
-                            update={update}
-                        />
-                    </div>
-
-                    <ImagesModalHotel hotel={hotel} />
-
-                </div>
-                <button
-                    className="cursor-pointer py-2 px-10 hover:bg-gray-600 bg-slate-900 text-white-0 hover:text-white-0 fonrt-bold uppercase rounded-lg"
-                    style={{
-                        position: "absolute",
-                        bottom: "20px",
-                        right: "10px",
-                    }}
-                    onClick={() => handleConfirm()}
-                >
-                    Save Edit
-                </button>
-            </ModalComponent>
-        </div>
-    )
-
+					<ImagesModalHotel hotel={hotel} />
+				</div>
+				<button
+					className="cursor-pointer py-2 px-10 hover:bg-gray-600 bg-slate-900 text-white-0 hover:text-white-0 fonrt-bold uppercase rounded-lg"
+					style={{
+						position: 'absolute',
+						bottom: '20px',
+						right: '10px'
+					}}
+					onClick={() => handleConfirm()}
+				>
+					Save Edit
+				</button>
+			</ModalComponent>
+		</div>
+	)
 }
