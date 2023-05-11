2fe19e230b674617ea6c48756bd8d252c23386fe
Ezequiel <oliverm316@gmail.com>
2023-05-11 14:31:41 +0200

    Alternative gifts added

--- src/screens/projects/render/gift/AddGiftButton.jsx
+++ src/screens/projects/render/gift/AddGiftButton.jsx
@@ -1,16 +1,10 @@
-export const AddGiftButton = ({ handleNavigate , gifts }) => {
-
-    //con esto hago que no se visible el button , esto es porque por ahora solamente se agrega un gift
-    if(gifts.length > 0){
-        return null    
-    }
-
-    return (
-        <button
-            onClick={() => handleNavigate()}
-            className="my-2 focus:scale-110 hover:animate-pulse bg-transparent hover:bg-orange-50 text-white-100 uppercase font-semibold hover:text-black-50 py-2 px-4 border border-orange-50 hover:border-transparent rounded"
-        >
-            Add Gift from DATABASE
-        </button>
-    )
+export const AddGiftButton = ({ handleNavigate }) => {
+	return (
+		<button
+			onClick={() => handleNavigate()}
+			className="my-2 focus:scale-110 hover:animate-pulse bg-transparent hover:bg-orange-50 text-white-100 uppercase font-semibold hover:text-black-50 py-2 px-4 border border-orange-50 hover:border-transparent rounded"
+		>
+			Add Gift from DATABASE
+		</button>
+	)
 }
--- src/screens/projects/render/gift/GiftSchedule.jsx
+++ src/screens/projects/render/gift/GiftSchedule.jsx
@@ -2,31 +2,32 @@ import { useNavigate } from 'react-router-dom'
 import { toast } from 'react-toastify'
 import { toastOptions } from '../../../../helper/toast'
 import { useCurrentProject } from '../../../../hooks'
-import { AddGiftButton } from "./AddGiftButton"
-import { GiftsCardList } from "./GiftsCardList"
+import { AddGiftButton } from './AddGiftButton'
+import { GiftsCardList } from './GiftsCardList'
 
 export const GiftSchedule = () => {
-    const navigate = useNavigate()
-    const { currentProject, removeGiftFromProject, editGift } = useCurrentProject()
+	const navigate = useNavigate()
+	const { currentProject, removeGiftFromProject, editGift } =
+		useCurrentProject()
 
-    const handleNavigate = () => {
-        navigate('/app/gift')
-    }
+	const handleNavigate = () => {
+		navigate('/app/gift')
+	}
 
-    const handleDeleteGift = (giftId) => {
-        removeGiftFromProject({ id: giftId })
-        toast.success('Gift Removed', toastOptions)
-    }
+	const handleDeleteGift = (giftId) => {
+		removeGiftFromProject({ id: giftId })
+		toast.success('Gift Removed', toastOptions)
+	}
 
-    return (
-        <div>
-            <h1 className="underline text-orange-200">GIFTS</h1>
-            <GiftsCardList
-                gifts={currentProject.gifts}
-                handleDeleteGift={handleDeleteGift}
-                editGif={editGift}
-            />
-            <AddGiftButton handleNavigate={handleNavigate} gifts={currentProject.gifts} />
-        </div>
-    )
+	return (
+		<div>
+			<h1 className="underline text-orange-200">GIFTS</h1>
+			<GiftsCardList
+				gifts={currentProject.gifts}
+				handleDeleteGift={handleDeleteGift}
+				editGif={editGift}
+			/>
+			<AddGiftButton handleNavigate={handleNavigate} />
+		</div>
+	)
 }
