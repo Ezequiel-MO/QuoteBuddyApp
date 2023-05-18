e2fc6f614481f7fd1071a71fb1a4dfaf191dcc92
edgardo90 <edgardo9000@gmail.com>
2023-05-18 17:27:44 -0300

    40_HOTEL_ADD_CARD

---
+++ src/screens/projects/render/hotel/CardAddHotel.jsx
@@ -0,0 +1,18 @@
+import { Icon } from '@iconify/react'
+import styles from '../DayEvents.module.css'
+
+export const CardAddHotel = ({navigate }) => {
+    return (
+        <div
+            className={styles.addHotel}
+            // onClick={() => navigate('/app/hotel')}
+        >
+            <h2 className={styles.textAddHotel}>
+                <span style={{ color: "white", marginRight: "10px", fontSize: "28px" }}>
+                    <Icon icon="bi:building-add" />
+                </span>
+                ADD HOTEL
+            </h2>
+        </div>
+    )
+}
