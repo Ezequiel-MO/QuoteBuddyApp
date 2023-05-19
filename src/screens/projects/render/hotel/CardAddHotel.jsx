b15c6a0c34e255a0699cc37c38b59c844530f2cb
edgardo90 <edgardo9000@gmail.com>
2023-05-19 00:10:27 -0300

    40_HOTEL_ADD_CARD

--- src/screens/projects/render/hotel/CardAddHotel.jsx
+++ src/screens/projects/render/hotel/CardAddHotel.jsx
@@ -5,7 +5,7 @@ export const CardAddHotel = ({navigate }) => {
     return (
         <div
             className={styles.addHotel}
-            // onClick={() => navigate('/app/hotel')}
+            onClick={() => navigate('/app/hotel')}
         >
             <h2 className={styles.textAddHotel}>
                 <span style={{ color: "white", marginRight: "10px", fontSize: "28px" }}>
