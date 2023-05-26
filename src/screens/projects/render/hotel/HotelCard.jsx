3548dbdd2ef95170aa50f084c6940d9a1eac7454
edgardo90 <edgardo9000@gmail.com>
2023-05-26 10:55:58 -0300

    dndkit_example

---
+++ src/screens/projects/render/hotel/HotelCard.jsx
@@ -0,0 +1,51 @@
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
+import { Icon } from '@iconify/react'
+import styles from '../DayEvents.module.css'
+
+export const HotelCard = ({ hotel, onDelete, handleClick, index }) => {
+    const { attributes, listeners, setNodeRef, transform, transition, isDragging } =
+        useSortable({ id: hotel.id });
+
+    const style = {
+        transform: CSS.Transform.toString(transform),
+        transition,
+        cursor: !isDragging ? "grab" : "grabbing",
+    };
+
+    return (
+        <div
+            className={styles.cardHotel}
+            style={style} // style del "dnd-kit" drag and drop
+            ref={setNodeRef}
+            {...attributes}
+        // {...listeners}
+        // onClick={(e) => handleClick(e, hotel, index)} // activa el modal hotel
+        >
+            <p
+                // style={style} // style del "dnd-kit" drag and drop
+                {...listeners}
+                className="truncate"
+            >
+                {hotel.name}
+            </p>
+            <span
+                className={styles.eyeIcon}
+                onClick={(e) => handleClick(e, hotel, index)}
+            >
+                <Icon icon="ic:baseline-remove-red-eye" />
+            </span>
+            <span
+                className={styles.deletedIcon}
+                onClick={(e) => {
+                    e.stopPropagation()
+                    onDelete(hotel._id)
+                }}
+            >
+                <Icon icon="lucide:delete" color="#ea5933" />
+            </span>
+        </div>
+    )
+}
+
+
--- src/screens/projects/render/hotel/HotelList.jsx
+++ src/screens/projects/render/hotel/HotelList.jsx
@@ -1,26 +1,134 @@
-import { useState } from 'react'
-import { HotelModal } from './hotelModal/HotelModal'
+// import { useState } from 'react'
+// import { HotelModal } from './hotelModal/HotelModal'
+// import { useCurrentProject } from '../../../../hooks'
+// import styles from '../DayEvents.module.css'
+// import { CardAdd, DraggingCard } from '../../../../components/atoms'
+
+// export const HotelList = ({ hotels, onDelete }) => {
+// 	const [open, setOpen] = useState(false)
+// 	const [hotelModal, setHotelModal] = useState()
+// 	const [hotelIndexModal, setIndexHotelModal] = useState()
+
+// 	const { dragAndDropHotel } = useCurrentProject()
+
+// 	const handleDragStart = (e, index) => {
+// 		e.dataTransfer.setData('hotelIndex', index)
+// 	}
+
+// 	const handleDrop = (e, index) => {
+// 		e.preventDefault()
+// 		const startHotelIndex = e.dataTransfer.getData('hotelIndex')
+// 		dragAndDropHotel({
+// 			startHotelIndex: Number(startHotelIndex),
+// 			endHotelIndex: index
+// 		})
+// 	}
+
+// 	const handleClick = (e, hotel, index) => {
+// 		setHotelModal(hotel)
+// 		setIndexHotelModal(index)
+// 		setOpen(true)
+// 	}
+
+// 	if (hotels.length === 0) {
+// 		return (
+// 			<div className={styles.hotels}>
+// 				<CardAdd
+// 					route="hotel"
+// 					name="hotel"
+// 					timeOfEvent={null}
+// 					dayOfEvent={null}
+// 				/>
+// 			</div>
+// 		)
+// 	}
+
+// 	return (
+// 		<div className={styles.hotels}>
+// 			<HotelModal
+// 				open={open}
+// 				setOpen={setOpen}
+// 				hotel={hotelModal}
+// 				index={hotelIndexModal}
+// 			/>
+
+// 			{hotels?.map((hotel, index) => (
+// 				<div key={hotel._id}>
+// 					<DraggingCard
+// 						item={hotel}
+// 						index={index}
+// 						handleDragStart={handleDragStart}
+// 						handleDrop={handleDrop}
+// 						handleDragEnd={null}
+// 						handleClick={handleClick}
+// 						onDelete={onDelete}
+// 					/>
+// 					{index === hotels.length - 1 && (
+// 						<CardAdd
+// 							route="hotel"
+// 							name="hotel"
+// 							timeOfEvent={null}
+// 							dayOfEvent={null}
+// 						/>
+// 					)}
+// 				</div>
+// 			))}
+// 		</div>
+// 	)
+// }
+
+
+//esto es una prueba
+import { useState, useEffect } from 'react';
 import { useCurrentProject } from '../../../../hooks'
+import { HotelModal } from './hotelModal/HotelModal'
+//importo para un nuevo drand and drop con "dndkit"
+import { DndContext, closestCenter } from "@dnd-kit/core"
+import { SortableContext, verticalListSortingStrategy, arrayMove } from "@dnd-kit/sortable"
+//hasta aca
+import { HotelCard } from "./HotelCard"
+import { CardAdd } from '../../../../components/atoms'
 import styles from '../DayEvents.module.css'
-import { CardAdd, DraggingCard } from '../../../../components/atoms'
 
 export const HotelList = ({ hotels, onDelete }) => {
+	const { dragAndDropHotel } = useCurrentProject()
+	const [hotelsState, setHotels] = useState([])
 	const [open, setOpen] = useState(false)
 	const [hotelModal, setHotelModal] = useState()
 	const [hotelIndexModal, setIndexHotelModal] = useState()
 
-	const { dragAndDropHotel } = useCurrentProject()
+	useEffect(() => {
+		const hotelsProject = hotels.map(el => {
+			const { _id, ...rest } = el
+			return {
+				...rest,
+				id: _id,
+				_id: _id
+			};
+		});
+		setHotels(hotelsProject)
+	}, [hotels])
 
-	const handleDragStart = (e, index) => {
-		e.dataTransfer.setData('hotelIndex', index)
-	}
-
-	const handleDrop = (e, index) => {
-		e.preventDefault()
-		const startHotelIndex = e.dataTransfer.getData('hotelIndex')
+	const handleDragEnd = (event) => {
+		// console.log(event)
+		// console.log("dragStart", active.data.current.sortable.index)
+		// console.log("dragOver", over.data.current.sortable.index)
+		// console.log("active", active.id);
+		// console.log("over", over.id);
+		const { active, over } = event;
+		const startHotelIndex = hotelsState.findIndex((el) => el.id === active.id);
+		const endHotelIndex = hotelsState.findIndex((el) => el.id === over.id);
+		// const startHotelIndex = active.data.current.sortable.index
+		// const endHotelIndex = over.data.current.sortable.index
+		// const copyHotels = [...hotelsState]
+		// const [hotelDragStart] = copyHotels.splice(startHotelIndex, 1)
+		// copyHotels.splice(endHotelIndex, 0, hotelDragStart)
+		// setHotels(copyHotels)
+		setHotels(arrayMove(hotelsState, startHotelIndex, endHotelIndex)) // arrayMove es una funcion  de dnd-kit
+		//se implementa el reducers
 		dragAndDropHotel({
 			startHotelIndex: Number(startHotelIndex),
-			endHotelIndex: index
+			endHotelIndex: Number(endHotelIndex)
 		})
 	}
 
@@ -49,30 +157,34 @@ export const HotelList = ({ hotels, onDelete }) => {
 				open={open}
 				setOpen={setOpen}
 				hotel={hotelModal}
-				index={hotelIndexModal}
 			/>
-
-			{hotels?.map((hotel, index) => (
-				<div key={hotel._id}>
-					<DraggingCard
-						item={hotel}
-						index={index}
-						handleDragStart={handleDragStart}
-						handleDrop={handleDrop}
-						handleDragEnd={null}
-						handleClick={handleClick}
-						onDelete={onDelete}
-					/>
-					{index === hotels.length - 1 && (
-						<CardAdd
-							route="hotel"
-							name="hotel"
-							timeOfEvent={null}
-							dayOfEvent={null}
+			<DndContext
+				collisionDetection={closestCenter}
+				onDragEnd={handleDragEnd}
+			>
+				<SortableContext
+					items={hotelsState}
+					strategy={verticalListSortingStrategy}
+				>
+					{hotelsState.map((hotel, index) => (
+						< HotelCard
+							key={hotel._id}
+							hotel={hotel}
+							onDelete={onDelete}
+							handleClick={handleClick}
+							index={index}
 						/>
-					)}
-				</div>
-			))}
+					))}
+				</SortableContext>
+			</DndContext>
+			<CardAdd
+				route="hotel"
+				name="hotel"
+				timeOfEvent={null}
+				dayOfEvent={null}
+			/>
 		</div>
-	)
+	);
 }
+
+
