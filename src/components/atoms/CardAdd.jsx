f6e36b1e90261f04174afa1bf58abbda5649eeba
Ezequiel <oliverm316@gmail.com>
2023-06-05 16:17:15 +0200

    CreateButton test

--- src/components/atoms/CardAdd.jsx
+++ src/components/atoms/CardAdd.jsx
@@ -1,24 +1,33 @@
+import PropTypes from 'prop-types'
 import { Icon } from '@iconify/react'
 import { useNavigate } from 'react-router-dom'
 
 export const CardAdd = ({ name, route, timeOfEvent, dayOfEvent }) => {
 	const navigate = useNavigate()
-	return (
-		<div
-			className="rounded-lg cursor-pointer border border-transparent bg-[#000] text-left w-[280px] flex items-center active:scale-95 active:transition active:duration-150 active:ease-in-out"
-			onClick={() =>
-				navigate(`/app/${route}`, {
-					state: {
-						timeOfEvent,
-						dayOfEvent
-					}
-				})
+	const handleClick = () =>
+		navigate(`/app/${route}`, {
+			state: {
+				timeOfEvent,
+				dayOfEvent
 			}
-		>
-			<h2 className="text-sm font-semibold flex items-center hover:bg-gray-600 hover:rounded-lg w-full">
+		})
+	const cardClassNames =
+		'rounded-lg cursor-pointer border border-transparent bg-[#000] text-left w-[280px] flex items-center active:scale-95 active:transition active:duration-150 active:ease-in-out'
+	const headerClassNames =
+		'text-sm font-semibold flex items-center hover:bg-gray-600 hover:rounded-lg w-full'
+	return (
+		<div className={cardClassNames} onClick={handleClick}>
+			<h2 className={headerClassNames}>
 				<Icon icon="bi:plus" width="30" className="text-orange-700" />
 				<span className="uppercase text-white-0 ">Add {name}</span>
 			</h2>
 		</div>
 	)
+}
+
+CardAdd.propTypes = {
+	name: PropTypes.string.isRequired,
+	route: PropTypes.string.isRequired,
+	timeOfEvent: PropTypes.string,
+	dayOfEvent: PropTypes.number
 }
