ff939b69c7d19ac27a036164a6e2a236d2b5e9cf
Ezequiel <oliverm316@gmail.com>
2023-06-07 19:15:14 +0200

    ModalCancelButton test

--- src/components/atoms/modal/ModalCancelButton.jsx
+++ src/components/atoms/modal/ModalCancelButton.jsx
@@ -2,13 +2,11 @@ import { Icon } from '@iconify/react'
 
 export const ModalCancelButton = ({ handleClose }) => {
 	return (
-		<div style={{marginBottom:"10px"}}>
-			<button
-				className="absolute top-2 right-2 cursor-pointer hover:scale-110 "
-				onClick={handleClose}
-			>
-				<Icon icon="material-symbols:cancel" width="30" color="#ea5933" />
-			</button>
-		</div>
+		<button
+			className="mb-3 top-2 right-2 cursor-pointer hover:scale-110 "
+			onClick={typeof handleClose === 'function' ? handleClose : undefined}
+		>
+			<Icon icon="material-symbols:cancel" width="30" color="#ea5933" />
+		</button>
 	)
 }
