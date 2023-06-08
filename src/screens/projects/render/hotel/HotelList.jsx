d46ec14fc0933a580b106e741776cee5796f11b4
Ezequiel <oliverm316@gmail.com>
2023-06-08 10:59:03 +0200

    in HotelList , Add Card even if hotels is empty - removed check

--- src/screens/projects/render/hotel/HotelList.jsx
+++ src/screens/projects/render/hotel/HotelList.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react'
 import { useCurrentProject } from '../../../../hooks'
 import { HotelModal } from './hotelModal/HotelModal'
-//importo para un nuevo drand and drop con "dndkit"
 import {
 	DndContext,
 	closestCenter,
@@ -15,7 +14,7 @@ import {
 	verticalListSortingStrategy,
 	arrayMove
 } from '@dnd-kit/sortable'
-//hasta aca
+
 import { HotelCard } from './HotelCard'
 import { CardAdd } from '../../../../components/atoms'
 import styles from '../DayEvents.module.css'
@@ -25,7 +24,6 @@ export const HotelList = ({ hotels, onDelete }) => {
 	const [hotelsState, setHotels] = useState([])
 	const [open, setOpen] = useState(false)
 	const [hotelModal, setHotelModal] = useState()
-	const [setIndexHotelModal] = useState()
 
 	const sensors = useSensors(useSensor(MouseSensor), useSensor(TouchSensor))
 
@@ -42,15 +40,11 @@ export const HotelList = ({ hotels, onDelete }) => {
 	}, [hotels])
 
 	const handleDragEnd = (event) => {
-		// console.log(event.delta)// obtiene las coordenadas XY del drag seleccionado
 		const { active, over } = event
 		const startHotelIndex = hotelsState.findIndex((el) => el.id === active.id)
 		const endHotelIndex = hotelsState.findIndex((el) => el.id === over.id)
-		// const copyHotels = [...hotelsState]
-		// const [hotelDragStart] = copyHotels.splice(startHotelIndex, 1)
-		// copyHotels.splice(endHotelIndex, 0, hotelDragStart)
-		// setHotels(copyHotels)
-		setHotels(arrayMove(hotelsState, startHotelIndex, endHotelIndex)) // arrayMove es una funcion  de dnd-kit
+
+		setHotels(arrayMove(hotelsState, startHotelIndex, endHotelIndex))
 		dragAndDropHotel({
 			startHotelIndex: Number(startHotelIndex),
 			endHotelIndex: Number(endHotelIndex)
@@ -59,21 +53,7 @@ export const HotelList = ({ hotels, onDelete }) => {
 
 	const handleClick = (e, hotel, index) => {
 		setHotelModal(hotel)
-		setIndexHotelModal(index)
 		setOpen(true)
-	}
-
-	if (hotels.length === 0) {
-		return (
-			<div className={styles.hotels}>
-				<CardAdd
-					route="hotel"
-					name="hotel"
-					timeOfEvent={null}
-					dayOfEvent={null}
-				/>
-			</div>
-		)
 	}
 
 	return (
