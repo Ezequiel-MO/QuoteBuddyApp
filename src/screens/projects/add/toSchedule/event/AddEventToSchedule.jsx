a0caac3e0845aa15cfc4915b8db63a581443154e
edgardo90 <edgardo9000@gmail.com>
2023-06-13 23:43:42 -0300

    #49_MODIFICATIONS

--- src/screens/projects/add/toSchedule/event/AddEventToSchedule.jsx
+++ src/screens/projects/add/toSchedule/event/AddEventToSchedule.jsx
@@ -3,7 +3,6 @@ import { useLocation, useNavigate } from 'react-router-dom'
 import { toast } from 'react-toastify'
 import { toastOptions } from '../../../../../helper/toast'
 import { useCurrentProject } from '../../../../../hooks'
-import { EventIntroForm } from '../../toEvent'
 import { EventTransfersForm } from '../../toEvent/inputs/EventTransfersForm'
 
 export const AddEventToSchedule = () => {
@@ -11,11 +10,7 @@ export const AddEventToSchedule = () => {
 	const navigate = useNavigate()
 	const { addEventToSchedule } = useCurrentProject()
 	const [event, setEvent] = useState(location.state.event)
-
-	const handleAddIntro = (intro) => {
-		event.introduction = [intro]
-		toast.success('Introduction added', toastOptions)
-	}
+	
 
 	const handleAddTransfer = (transferObj, selectedService, nrVehicles) => {
 		const transferData = { ...transferObj, selectedService }
@@ -23,28 +18,30 @@ export const AddEventToSchedule = () => {
 		for (let i = 0; i < nrVehicles; i++) {
 			transfers.push(transferData)
 		}
-		setEvent({ ...event, transfer: transfers })
-		toast.success('Transfer added', toastOptions)
+		let addEvent = {}
+		setEvent(currentEvent => {
+			const newEvent = { ...currentEvent, transfer: transfers }
+			addEvent = newEvent
+			return newEvent
+		});
+		if (addEvent?.transfer && addEvent?.transfer.length > 0) {
+			toast.success('Transfer added', toastOptions)
+		}
+		setTimeout(() => {
+			addEventToSchedule({
+				dayOfEvent: location.state.dayOfEvent,
+				timeOfEvent: location.state.timeOfEvent,
+				event: addEvent
+			})
+			toast.success('Event Added to Schedule', toastOptions)
+			navigate('/app/project/schedule')
+		}, 1000)
 	}
 
-	/* const handleAddVenuePrices = (prices) => {
-		setEvent({ ...event, venue_price: [prices] })
-		toast.success('Venue prices added', toastOptions)
-	} */
-
-	const handleAddEvent = () => {
-		addEventToSchedule({
-			dayOfEvent: location.state.dayOfEvent,
-			timeOfEvent: location.state.timeOfEvent,
-			event
-		})
-		toast.success('Event Added to Schedule', toastOptions)
-		navigate('/app/project/schedule')
-	}
 
 	return (
-		<div className="grid grid-cols-2" style={{marginTop:"20px"}}>
-				<EventTransfersForm handleAddTransfer={handleAddTransfer} />
+		<div className="grid grid-cols-2" style={{ marginTop: "20px" }}>
+			<EventTransfersForm handleAddTransfer={handleAddTransfer} />
 		</div>
 	)
 }
