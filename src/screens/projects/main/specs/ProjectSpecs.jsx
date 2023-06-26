07f62d79e34947acd69c1eb65e3d2b2f1279971e
Ezequiel <oliverm316@gmail.com>
2023-06-26 12:52:46 +0200

    onSuccess, onError added to ProjectSpecs

--- src/screens/projects/main/specs/ProjectSpecs.jsx
+++ src/screens/projects/main/specs/ProjectSpecs.jsx
@@ -2,14 +2,30 @@ import { useRef } from 'react'
 import { useLocation } from 'react-router-dom'
 import { ProjectMasterForm } from '../form/ProjectMasterForm'
 import { usePostToEndpoint } from './usePostToEndpoint'
+import { useCurrentProject } from '../../../../hooks'
+import { toast } from 'react-toastify'
+import { toastOptions } from '../../../../helper/toast'
 
 export const ProjectSpecs = () => {
 	const {
 		state: { project }
 	} = useLocation()
 	const fileInput = useRef()
+	const { setCurrentProject } = useCurrentProject()
 
-	const postToEndpoint = usePostToEndpoint(project)
+	const onSuccess = (data, update) => {
+		localStorage.setItem('currentProject', JSON.stringify(data))
+		setCurrentProject(data)
+		toast.success(`Project ${update ? 'updated' : 'created'}`, toastOptions)
+	}
+	const onError = (error) => {
+		if (error.includes('clientAccManager')) {
+			return toast.error(`Have not selected Client Acc.Manager`, toastOptions)
+		}
+		toast.error(`${error}`, toastOptions)
+	}
+
+	const postToEndpoint = usePostToEndpoint(project, onSuccess, onError)
 
 	const submitForm = (values, files, endPoint, update, open) => {
 		postToEndpoint(values, files, endPoint, update, open)
--- src/screens/projects/main/specs/usePostToEndpoint.js
+++ src/screens/projects/main/specs/usePostToEndpoint.js
@@ -1,14 +1,13 @@
 import { toast } from 'react-toastify'
 import baseAPI from '../../../../axios/axiosConfig'
 import { computeTotalDays } from '../../../../helper/helperFunctions'
-import { useCurrentProject } from '../../../../hooks'
 import { transformData, updatePdf, updatedData } from './transformData'
-import { errorToastOptions, toastOptions } from '../../../../helper/toast'
+import { errorToastOptions } from '../../../../helper/toast'
 import { useNavigate } from 'react-router-dom'
 
-export const usePostToEndpoint = (project) => {
+export const usePostToEndpoint = (project, onSuccess, onError) => {
 	const navigate = useNavigate()
-	const { setCurrentProject } = useCurrentProject()
+
 	const postToEndpoint = async (data, files, endPoint, update, open) => {
 		const diffDays = computeTotalDays(data.arrivalDay, data.departureDay)
 		//se verifica que  "company" tenga ese "employee"
@@ -27,19 +26,34 @@ export const usePostToEndpoint = (project) => {
 		})
 
 		try {
+			if (update) {
+				const { formData, updateTransformedData } = updatedData({
+					files: files,
+					open: open,
+					data: data
+				})
+				const res = await baseAPI.patch(
+					`projects/${project._id}`,
+					updateTransformedData
+				)
+				onSuccess(res.data.data.data, true)
+
+				// si se hace un update y no habia ningun pdf guardado, hace un patch guardar el pdf en otra peticion
+				if (files.length > 0 && endPoint !== 'projects/image') {
+					await baseAPI.patch(
+						`projects/images/${res.data.data.data._id}`,
+						formData
+					)
+				}
+			}
 			if (!update) {
 				const res = await baseAPI.post(`${endPoint}`, transformedData)
-				localStorage.setItem(
-					'currentProject',
-					JSON.stringify(res.data.data.data)
-				)
-				setCurrentProject(res.data.data.data)
+				onSuccess(res.data.data.data, true)
 				// una vez que se crea hace un patch guardar el pdf en otra peticion
 				await baseAPI.patch(
 					`${endPoint}/images/${res.data.data.data._id}`,
 					formData
 				)
-				toast.success('Base Project Created', toastOptions)
 			}
 			if (endPoint === 'projects/image') {
 				const { formData, allFiles } = updatePdf({ values: data, files: files })
@@ -51,38 +65,11 @@ export const usePostToEndpoint = (project) => {
 				}
 				await baseAPI.patch(`projects/images/${project._id}`, formData)
 			}
-			if (update) {
-				const { formData, updateTransformedData } = updatedData({
-					files: files,
-					open: open,
-					data: data
-				})
-				const res = await baseAPI.patch(
-					`projects/${project._id}`,
-					updateTransformedData
-				)
-				localStorage.setItem(
-					'currentProject',
-					JSON.stringify(res.data.data.data)
-				)
-				setCurrentProject(res.data.data.data)
-				// si se hace un update y no habia ningun pdf guardado, hace un patch guardar el pdf en otra peticion
-				if (files.length > 0 && endPoint !== 'projects/image') {
-					await baseAPI.patch(
-						`projects/images/${res.data.data.data._id}`,
-						formData
-					)
-				}
-				toast.success('Project updated', toastOptions)
-			}
 			setTimeout(() => {
 				navigate('/app')
 			}, 1000)
 		} catch (error) {
-			if (error.response.data.message.includes('clientAccManager')) {
-				return toast.error(`Have not selected Client Acc.Manager`, toastOptions)
-			}
-			toast.error(`${error.response.data.message}`, toastOptions)
+			onError(error.response.data.message)
 		}
 	}
 
