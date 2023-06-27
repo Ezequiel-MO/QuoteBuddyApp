7aeea11d0f5469d89678ec303c6b2d2807601d96
Ezequiel <oliverm316@gmail.com>
2023-06-27 14:43:18 +0200

    ScheduleTableRow extracted from TableSchedule

---
+++ src/screens/projects/render/schedule/render/ScheduleTableRow.jsx
@@ -0,0 +1,70 @@
+import { DayEvents } from './DayEvents'
+import { DayMeals } from './DayMeals'
+
+export const ScheduleTableRow = ({
+	day,
+	index,
+	handleDeleteEvent,
+	legacyProject
+}) => (
+	<tr key={day._id} className="border border-white-100">
+		<td>{day.date}</td>
+		<td>
+			<DayEvents
+				day={day}
+				event="morningEvents"
+				handleDeleteEvent={handleDeleteEvent}
+				dayIndex={index}
+			/>
+			<DayEvents
+				day={day}
+				event="morningMeetings"
+				handleDeleteEvent={handleDeleteEvent}
+				dayIndex={index}
+				renderAddCard={false}
+			/>
+		</td>
+		<td>
+			<DayMeals
+				day={day}
+				event="lunch"
+				handleDeleteEvent={handleDeleteEvent}
+				dayIndex={index}
+				legacyProject={legacyProject}
+			/>
+		</td>
+		<td>
+			<DayEvents
+				day={day}
+				event="afternoonEvents"
+				handleDeleteEvent={handleDeleteEvent}
+				dayIndex={index}
+			/>
+			<DayEvents
+				day={day}
+				event="afternoonMeetings"
+				handleDeleteEvent={handleDeleteEvent}
+				dayIndex={index}
+				renderAddCard={false}
+			/>
+		</td>
+		<td>
+			<DayMeals
+				day={day}
+				event="dinner"
+				handleDeleteEvent={handleDeleteEvent}
+				dayIndex={index}
+				legacyProject={legacyProject}
+			/>
+		</td>
+		<td>
+			<DayEvents
+				day={day}
+				event="fullDayMeetings"
+				handleDeleteEvent={handleDeleteEvent}
+				dayIndex={index}
+				renderAddCard={false}
+			/>
+		</td>
+	</tr>
+)
--- src/screens/projects/render/schedule/render/TableSchedule.jsx
+++ src/screens/projects/render/schedule/render/TableSchedule.jsx
@@ -3,8 +3,6 @@ import { toast } from 'react-toastify'
 import { toastOptions } from '../../../../../helper/toast'
 import { useCurrentProject } from '../../../../../hooks'
 import { TableHeaders } from '../../../../../ui'
-import { DayEvents } from './DayEvents'
-import { DayMeals } from './DayMeals'
 
 import {
 	DndContext,
@@ -19,6 +17,7 @@ import {
 } from '@dnd-kit/core'
 import { arrayMove, sortableKeyboardCoordinates } from '@dnd-kit/sortable'
 import { EventActivate } from './card/EventActivate'
+import { ScheduleTableRow } from './ScheduleTableRow'
 
 export const TableSchedule = () => {
 	const { currentProject, removeEventFromSchedule, dragAndDropEvent } =
@@ -152,69 +151,13 @@ export const TableSchedule = () => {
 			>
 				<tbody>
 					{events?.map((day, index) => (
-						<tr key={day._id} className="border border-white-100">
-							<td>{day.date}</td>
-							<td>
-								<DayEvents
-									day={day}
-									event="morningEvents"
-									handleDeleteEvent={handleDeleteEvent}
-									dayIndex={index}
-								/>
-
-								<DayEvents
-									day={day}
-									event="morningMeetings"
-									handleDeleteEvent={handleDeleteEvent}
-									dayIndex={index}
-									renderAddCard={false}
-								/>
-							</td>
-
-							<td>
-								<DayMeals
-									day={day}
-									event="lunch"
-									handleDeleteEvent={handleDeleteEvent}
-									dayIndex={index}
-									legacyProject={legacyProject}
-								/>
-							</td>
-							<td>
-								<DayEvents
-									day={day}
-									event="afternoonEvents"
-									handleDeleteEvent={handleDeleteEvent}
-									dayIndex={index}
-								/>
-								<DayEvents
-									day={day}
-									event="afternoonMeetings"
-									handleDeleteEvent={handleDeleteEvent}
-									dayIndex={index}
-									renderAddCard={false}
-								/>
-							</td>
-
-							<td>
-								<DayMeals
-									day={day}
-									event="dinner"
-									handleDeleteEvent={handleDeleteEvent}
-									dayIndex={index}
-									legacyProject={legacyProject}
-								/>
-							</td>
-							<td>
-								<DayEvents
-									day={day}
-									event="fullDayMeetings"
-									handleDeleteEvent={handleDeleteEvent}
-									dayIndex={index}
-									renderAddCard={false}
-								/>
-							</td>
-						</tr>
+						<ScheduleTableRow
+							key={day._id}
+							day={day}
+							index={index}
+							handleDeleteEvent={handleDeleteEvent}
+							legacyProject={legacyProject}
+						/>
 					))}
 				</tbody>
 				<DragOverlay>
