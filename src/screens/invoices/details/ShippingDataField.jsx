f97d538e8fa77afae92df54e17aee035822530e6
Ezequiel <oliverm316@gmail.com>
2023-07-03 16:02:11 +0200

    Restyling of Invoice data

--- src/screens/invoices/details/ShippingDataField.jsx
+++ src/screens/invoices/details/ShippingDataField.jsx
@@ -5,18 +5,18 @@ export const ShippingDataField = ({
 	handleChange,
 	isEditable
 }) => (
-	<div className="font-bold leading-none flex">
+	<div className="my-1 w-[700px] font-bold text-lg flex justify-between items-center bg-gray-200 p-4 rounded-md">
 		{label}:
 		{isEditable ? (
 			<input
 				type={`${name === 'date' ? 'date' : 'text'}`}
 				name={name}
-				className="ml-2 font-normal cursor-pointer w-[500px]"
+				className="ml-2 font-normal text-gray-700 cursor-pointer w-full rounded-md border border-gray-300 p-2"
 				value={value}
 				onChange={handleChange}
 			/>
 		) : (
-			<p className="ml-2 font-normal">{value}</p>
+			<p className="ml-2 text-gray-700">{value}</p>
 		)}
 	</div>
 )
