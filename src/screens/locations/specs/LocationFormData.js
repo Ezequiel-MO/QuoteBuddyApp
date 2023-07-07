55b60b4eb29c5e336ab280cb65236d0e4adf5934
Ezequiel <oliverm316@gmail.com>
2023-07-07 15:33:02 +0200

    locations functionality to update images up

---
+++ src/screens/locations/specs/LocationFormData.js
@@ -0,0 +1,41 @@
+export const LocationFormData = {
+	create: (values, files) => {
+		let formData = new FormData()
+		formData.append('name', values.name)
+		formData.append('textContent', values.textContent)
+		formData.append('location[coordinates][0]', values.latitude)
+		formData.append('location[coordinates][1]', values.longitude)
+		if (files.length > 0) {
+			for (let i = 0; i < files.length; i++) {
+				formData.append('imageContentUrl', files[i])
+			}
+		}
+		return formData
+	},
+	update: (values) => {
+		const jsonData = {}
+		jsonData.name = values.name
+		jsonData.textContent = values.textContent
+		jsonData.location = {
+			type: 'Point',
+			coordinates: [values.latitude, values.longitude]
+		}
+
+		return jsonData
+	},
+	updateImageData: (values, files) => {
+		let formData = new FormData()
+		if (values?.imageContentUrl.length > 0) {
+			formData.append('imageUrls', values.imageContentUrl)
+		}
+		if (values?.deletedImage?.length > 0) {
+			formData.append('deletedImage', values.deletedImage)
+		}
+		if (files.length > 0) {
+			for (let i = 0; i < files.length; i++) {
+				formData.append('imageContentUrl', files[i])
+			}
+		}
+		return formData
+	}
+}
--- src/screens/locations/specs/LocationSpecs.jsx
+++ src/screens/locations/specs/LocationSpecs.jsx
@@ -3,6 +3,7 @@ import { toast } from 'react-toastify'
 import baseAPI from '../../../axios/axiosConfig'
 import { errorToastOptions, toastOptions } from '../../../helper/toast'
 import LocationMasterForm from './LocationMasterForm'
+import { LocationFormData } from './LocationFormData'
 
 const LocationSpecs = () => {
 	const navigate = useNavigate()
@@ -13,44 +14,24 @@ const LocationSpecs = () => {
 
 	const update = Object.keys(location).length > 0 ? true : false
 
-	const fillFormData = (values, files) => {
-		let formData = new FormData()
-		formData.append('name', values.name)
-		formData.append('textContent', values.textContent)
-		formData.append('location[coordinates][0]', values.latitude)
-		formData.append('location[coordinates][1]', values.longitude)
-		if (files.length > 0) {
-			for (let i = 0; i < files.length; i++) {
-				formData.append('imageContentUrl', files[i])
-			}
-		}
-		return formData
-	}
-
-	const fillJSONData = (values) => {
-		let jsonData = {}
-		jsonData.name = values.name
-		jsonData.textContent = values.textContent
-		jsonData.location = {
-			type: 'Point',
-			coordinates: [values.latitude, values.longitude]
-		}
-
-		return jsonData
-	}
-
 	const submitForm = async (values, files, endpoint, update) => {
 		let dataToPost
 		try {
-			if (update === false) {
-				dataToPost = fillFormData(values, files)
-				await baseAPI.post('locations', dataToPost)
-				toast.success('Location Created', toastOptions)
-			} else {
-				dataToPost = fillJSONData(values)
+			if (update) {
+				dataToPost = LocationFormData.update(values)
 				await baseAPI.patch(`locations/${location._id}`, dataToPost)
 				toast.success('Location Updated', toastOptions)
 			}
+			if (!update) {
+				dataToPost = LocationFormData.create(values, files)
+				await baseAPI.post('locations', dataToPost)
+				toast.success('Location Created', toastOptions)
+			}
+			if (endpoint === 'locations/image') {
+				dataToPost = LocationFormData.updateImageData(values, files)
+				await baseAPI.patch(`locations/images/${location._id}`, dataToPost)
+			}
+
 			setTimeout(() => {
 				navigate('/app/location')
 			}, 1000)
