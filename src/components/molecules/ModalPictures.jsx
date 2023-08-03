25ec3737a91f908d736a5147113c535f22b6050d
edgardo90 <edgardo9000@gmail.com>
2023-08-03 13:42:11 -0300

    63_IMAGES_VISIBLE

--- src/components/molecules/ModalPictures.jsx
+++ src/components/molecules/ModalPictures.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react'
+import { useRef, useState, useEffect, useMemo } from 'react'
 import { Form, Formik } from 'formik'
 import { Icon } from '@iconify/react'
 import pdfLogo from '../../assets/pdf_logo.jpg'
@@ -14,18 +14,65 @@ export const ModalPictures = ({
 	nameScreen,
 	multipleCondition
 }) => {
-	const [imgList, setImgList] = useState([])
-	const [isUpdate, setIsUpdate] = useState(false)
-
 	const fileInput = useRef()
-
+	const [imagePreviewUrls, setImagePreviewUrls] = useState([])
+	const [filesImages, setFilesImages] = useState([])
+	const [deletedImage, setDeletedImage] = useState([])
 	const update = Object.keys(screen).length > 0 ? true : false
+
+	useEffect(() => {
+		setFilesImages([])
+		setDeletedImage([])
+		if (update && screen?.imageContentUrl.length > 0) {
+			const images = [...screen.imageContentUrl]
+			const imageUrls = images.map(el => {
+				return {
+					url: el,
+					name: el
+				}
+			})
+			setImagePreviewUrls(imageUrls)
+		}
+	}, [screen, open])
+
+
+	const handleUploadImages = () => {
+		const files = Array.from(fileInput.current.files) // Convierte FileList en un array "[...fileInput.current.files] con spread operator tambien funciona"
+		const imageUrls = files.map(file => {
+			return {
+				url: URL.createObjectURL(file), // Crea URLs para cada archivo , va servir para renderizar lo que se sube
+				name: file.name  // nombre real de la imagen , lo guardo para eleminarla del estado "filesImages"
+			}
+		})
+		setImagePreviewUrls([...imagePreviewUrls, ...imageUrls]) // Almacena las URLs en el estado
+		setFilesImages([...filesImages, ...fileInput.current.files])
+	}
+
+	const handleDeletedImage = (e, imagenUrl) => {
+		const newImageUrls = imagePreviewUrls.filter(el => el.url !== imagenUrl.url)
+		const newFilesImages = filesImages.filter(el => el.name !== imagenUrl.name)
+		setImagePreviewUrls(newImageUrls)
+		setFilesImages(newFilesImages)
+		//si es una imagen de amazon web service lo mando al estado "deletedImage"
+		if (imagenUrl?.url.includes("amazonaws")) {
+			setDeletedImage([...deletedImage, imagenUrl?.url])
+		}
+	}
+
+	const imageContentUrl = useMemo(() => {
+		return imagePreviewUrls.map((el) => {
+			if (el?.url.includes("amazon")) {
+				return el.url
+			}
+		}).filter(el => el)
+	}, [imagePreviewUrls])
+	
 
 	return (
 		<>
 			<ModalComponent open={open} setOpen={setOpen}>
-				<ImageList sx={{ width: 520, height: 450 }} cols={3} rowHeight={164}>
-					{screen?.imageContentUrl?.map((item, index) => (
+				<ImageList sx={{ width: 590, height: 450 }} cols={3} rowHeight={164}>
+					{imagePreviewUrls?.map((item, index) => (
 						<ImageListItem key={index} style={{ position: 'relative' }}>
 							<div
 								style={{
@@ -34,22 +81,14 @@ export const ModalPictures = ({
 									color: 'red',
 									margin: '1px'
 								}}
-								onClick={() => {
-									const arr = [...imgList]
-									if (index > -1) {
-										arr.push(screen.imageContentUrl[index])
-										setImgList(arr)
-										screen.imageContentUrl.splice(index, 1) // 2nd parameter means remove one item only
-									}
-									setIsUpdate(!isUpdate)
-								}}
+								onClick={(e) => handleDeletedImage(e, item)}
 							>
 								<Icon icon="material-symbols:cancel" width="30" />
 							</div>
-							{!item?.toLowerCase().includes('pdf') ? (
+							{!item?.url.toLowerCase().includes('pdf') ? (
 								<img
-									src={`${item}?w=164&h=164&fit=crop&auto=format`}
-									srcSet={`${item}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+									className="w-40 h-40 object-cover object-center"
+									src={item.url}
 									loading="lazy"
 								/>
 							) : (
@@ -62,11 +101,11 @@ export const ModalPictures = ({
 					<Formik
 						initialValues={initialValues}
 						onSubmit={(values) => {
-							values['imageContentUrl'] = screen.imageContentUrl
-							values['deletedImage'] = imgList
+							values['imageContentUrl'] = imageContentUrl
+							values['deletedImage'] = deletedImage
 							submitForm(
 								values,
-								fileInput.current.files ?? [],
+								filesImages ?? [],
 								`${nameScreen}/image`,
 								update
 							)
@@ -77,17 +116,18 @@ export const ModalPictures = ({
 								<Form>
 									<fieldset className="grid grid-cols-2 gap-4">
 										<div className="flex align-center justify-start">
-											<label
-												htmlFor="file-upload"
-												className="custom-file-upload"
-											></label>
 											<input
+												style={{ color: "transparent" }}
 												id="file-upload"
 												type="file"
 												ref={fileInput}
 												name="imageContentUrl"
 												multiple={multipleCondition}
+												onChange={handleUploadImages}
 											/>
+											<span style={{position:"absolute", marginTop:"30px"}} >
+												{`${filesImages.length} files selected for upload`}
+											</span>
 										</div>
 										<input
 											type="submit"
