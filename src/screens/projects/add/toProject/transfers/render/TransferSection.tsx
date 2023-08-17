9270db99396cab9ea089bf33255e00e3e408b1af
edgardo90 <edgardo9000@gmail.com>
2023-08-17 00:27:40 -0300

    69_EDIT_TRANSFERS_IN

--- src/screens/projects/add/toProject/transfers/render/TransferSection.tsx
+++ src/screens/projects/add/toProject/transfers/render/TransferSection.tsx
@@ -1,27 +1,52 @@
+import { useEffect } from "react"
 import { Icon } from '@iconify/react'
-import { useTransfers } from './context'
+import { useTransfers, } from './context'
 import { Button } from '@mui/material'
-import {REMOVE_TRANSFER_IN , REMOVE_TRANSFER_OUT} from "./actionTypes"
+import { REMOVE_TRANSFER_IN, REMOVE_TRANSFER_OUT } from "./actionTypes"
+import { useCurrentProject } from '../../../../../../hooks'
+import { ITransfer } from '../../../../../../interfaces'
 
 export const TransferSection = () => {
-	const { state, dispatch , typeTransfer } = useTransfers()
-	const { transfersIn , transfersOut } = state
-	const transfersRender = typeTransfer === "in" ? transfersIn : transfersOut
+	const { state, dispatch, typeTransfer } = useTransfers()
+	const { transfersIn, transfersOut } = state
+	const { currentProject } = useCurrentProject()
+	let transfersRender = typeTransfer === "in" ? transfersIn : transfersOut
 
-	const handleDeletedTransfer = (index:number) =>{
-		if(typeTransfer === "in"){
+	useEffect(() => {
+		if (typeTransfer === "in" && transfersRender.length === 0) {
+			const transfersInProject: ITransfer[] = currentProject?.schedule[0]?.transfer_in
+			dispatch({
+				type: "UPDATE_TRANSFER_IN",
+				payload: { transferObject: transfersInProject }
+			})
+		}
+		if (typeTransfer === "out" && transfersRender.length === 0) {
+			const lastIndex = currentProject?.schedule.length - 1
+			const transfersOutProject: ITransfer[] = currentProject?.schedule[lastIndex]?.transfer_out
+			dispatch({
+				type: "UPDATE_TRANSFER_OUT",
+				payload: { transferObject: transfersOutProject }
+			})
+		}
+	}, [typeTransfer])
+
+
+	const handleDeletedTransfer = (index: number) => {
+		if (typeTransfer === "in") {
 			dispatch({
 				type: REMOVE_TRANSFER_IN,
 				payload: index
 			})
 		}
-		if(typeTransfer === "out"){
+		if (typeTransfer === "out") {
 			dispatch({
 				type: REMOVE_TRANSFER_OUT,
 				payload: index
 			})
 		}
 	}
+
+	if (transfersRender.length === 0) return null
 
 	return (
 		<div className="my-2 bg-slate-400 text-white-0 p-6 rounded-lg shadow-md">
@@ -43,12 +68,7 @@ export const TransferSection = () => {
 							<span className="text-gray-800 ml-2">Transfer {typeTransfer}</span>
 						</div>
 						<Button
-							onClick={() =>  handleDeletedTransfer(index)
-								// dispatch({
-								// 	type: REMOVE_TRANSFER_IN,
-								// 	payload: index
-								// })
-							}
+							onClick={() => handleDeletedTransfer(index)}
 						>
 							<Icon
 								icon="fluent:delete-24-regular"
--- src/screens/projects/add/toProject/transfers/render/TransfersModalBody.tsx
+++ src/screens/projects/add/toProject/transfers/render/TransfersModalBody.tsx
@@ -1,11 +1,7 @@
 import { ServiceSection } from './ServiceSection'
 import { TransferSection } from './TransferSection'
-import { useTransfers } from './context'
 
 export const TransfersModalBody = () => {
-	const { selectedSection } = useTransfers()
-
-	if (!selectedSection) return null
 
 	return (
 		<>
