86efee150f418edfa8537ee5ac5bb7dc93b72a15
edgardo90 <edgardo9000@gmail.com>
2023-08-24 00:07:20 -0300

    72_ADD_RESTAURANT_TRANSFERS

--- src/helper/toast.tsx
+++ src/helper/toast.tsx
@@ -1,11 +1,11 @@
 export const toastOptions = {
-	position: 'top-right',
+	position: 'top-right' as const,
 	autoClose: 750,
 	hideProgressBar: false,
 	closeOnClick: true,
 	pauseOnHover: true,
 	draggable: true,
-	progress: undefined
+	// progress: undefined
 }
 
 export const errorToastOptions = {
