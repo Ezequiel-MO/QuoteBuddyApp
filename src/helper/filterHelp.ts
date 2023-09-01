e860a50d63df072978a7d25ac425656692e8c515
Ezequiel <oliverm316@gmail.com>
2023-09-01 11:39:30 +0200

    filter function refactored to be more readable

--- src/helper/filterHelp.ts
+++ src/helper/filterHelp.ts
@@ -12,6 +12,28 @@ interface FilterParams {
 	includePagination?: boolean
 }
 
+const appendPagination = (url: string, page: number, limit: string) => {
+	return `${url}?page=${page}&limit=${limit}`
+}
+
+const filterValues = (
+	valuesRute: FilterValue[],
+	filterOptions: string[]
+): FilterValue[] => {
+	return valuesRute.filter(
+		({ name, value }) =>
+			filterOptions.some((option) => name.includes(option)) &&
+			value !== undefined
+	)
+}
+
+const appendFiltersToUrl = (url: string, valuesUrlFilters: FilterValue[]) => {
+	const filterString = valuesUrlFilters
+		.map(({ name, value }) => `&${name}=${value}`)
+		.join('')
+	return `${url}${filterString}`
+}
+
 export const filter = ({
 	valuesRute,
 	url,
@@ -19,29 +41,11 @@ export const filter = ({
 	page,
 	limit = '10',
 	includePagination = true
-}: FilterParams) => {
-	let resultsUrl = includePagination
-		? `${url}?page=${page}&limit=${limit}`
-		: url
-	const valuesUrlFilters = []
-	for (let i = 0; i < valuesRute.length; i++) {
-		for (let j = 0; j < filterOptions.length; j++) {
-			if (valuesRute[i].name.includes(filterOptions[j])) {
-				valuesRute[i].value && valuesUrlFilters.push(valuesRute[i])
-				typeof valuesRute[i].value === 'boolean' &&
-					valuesUrlFilters.push(valuesRute[i])
-			}
-		}
-	}
-	let newUrl = ''
-	if (valuesUrlFilters.length > 0) {
-		for (let i = 0; i < valuesUrlFilters.length; i++) {
-			newUrl =
-				newUrl + `&${valuesUrlFilters[i].name}=${valuesUrlFilters[i].value}`
-		}
-	}
-	let finalUrl = resultsUrl + newUrl
-	return finalUrl
+}: FilterParams): string => {
+	let resultsUrl = includePagination ? appendPagination(url, page, limit) : url
+
+	const valuesUrlFilters = filterValues(valuesRute, filterOptions)
+	return appendFiltersToUrl(resultsUrl, valuesUrlFilters)
 }
 
 interface FilterDocumentLengthParams {
