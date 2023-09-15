0d7eae0fa1f412bbd2c3c6e6664347daf3367143
Ezequiel <oliverm316@gmail.com>
2023-09-15 13:43:34 +0200

    small fix

--- src/axios/axiosConfig.ts
+++ src/axios/axiosConfig.ts
@@ -1,18 +1,19 @@
 import axios, { AxiosInstance } from 'axios'
 
-// INTERFACE GLOBAL PARA "env" 
+// INTERFACE GLOBAL PARA "env"
 declare global {
 	interface ImportMeta {
-	  env: {
-		VITE_BACKEND_URL: string;
-	  };
+		env: {
+			VITE_BACKEND_URL: string
+		}
 	}
-  }
+}
 
 const baseAPI: AxiosInstance = axios.create({
 	// baseURL: 'http://localhost:3000/v2',
 	baseURL: `${import.meta.env.VITE_BACKEND_URL}`,
 	headers: {
+		'Content-Type': 'application/json',
 		Authorization: `Bearer ${localStorage.getItem('token')}`
 	}
 })
