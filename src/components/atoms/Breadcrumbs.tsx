61b6e289fb2bfbc756b52f0e3b843341ba171abd
Ezequiel <oliverm316@gmail.com>
2023-09-25 14:04:50 +0200

    Breadcrumbs rearranged

---
+++ src/components/atoms/Breadcrumbs.tsx
@@ -0,0 +1,31 @@
+// Breadcrumbs.tsx
+import React from 'react'
+import { Link, useLocation } from 'react-router-dom'
+
+export const Breadcrumbs: React.FC = () => {
+	const location = useLocation()
+	let currentLink = ''
+
+	const crumbs = location.pathname
+		.split('/')
+		.filter((crumb) => crumb !== '')
+		.map((crumb, index, array) => {
+			currentLink += `/${crumb}`
+			return (
+				<div key={`${crumb}-${index}`} className="flex items-center">
+					<Link to={currentLink} className="text-slate-800 hover:underline">
+						{crumb}
+					</Link>
+					{index !== array.length - 1 && (
+						<span className="mx-2 text-slate-800">/</span>
+					)}
+				</div>
+			)
+		})
+
+	return (
+		<div className="flex flex-row items-center mt-3 font-semibold text-xl text-gray-700">
+			{crumbs}
+		</div>
+	)
+}
