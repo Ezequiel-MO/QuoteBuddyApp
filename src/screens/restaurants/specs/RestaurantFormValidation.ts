752d510ba99ffec57ffdd97ac77124101177dcad
edgardo90 <edgardo9000@gmail.com>
2023-10-07 13:20:04 -0300

    91_FORMS_RESTAURANTS_REFACTOR

--- src/screens/restaurants/specs/RestaurantFormValidation.ts
+++ src/screens/restaurants/specs/RestaurantFormValidation.ts
@@ -6,7 +6,7 @@ export const getValidationSchema = () => {
 		city: Yup.string().required('Required'),
 		longitude: Yup.number().required('Required'),
 		latitude: Yup.number().required('Required'),
-		price: Yup.number().required('Required'),
-		isVenue: Yup.boolean()
+		price: Yup.number().typeError('Required').min(1, 'Required')
+		// isVenue: Yup.boolean()
 	})
 }
--- src/screens/restaurants/specs/RestaurantMasterForm.tsx
+++ src/screens/restaurants/specs/RestaurantMasterForm.tsx
@@ -1,12 +1,12 @@
-import { useState, useRef } from 'react'
-import { Form, Formik } from 'formik'
-import { useGetLocations, useImageState, usePdfState } from '../../../hooks'
+import { useState, useRef, useEffect } from 'react'
+import { useImageState, usePdfState, useFormHandling } from '../../../hooks'
 import { ModalPictures, AddImagesModal, ModalPdf, AddPdfModal } from '../../../components/molecules'
 import { getValidationSchema, RestaurantFormFields } from '..'
-import { ShowImagesButton } from '../../../components/atoms'
+import { ShowImagesButton, SubmitInput } from '../../../components/atoms'
 import { generateFormValues } from '../../../helper'
-import { formsValues } from '../../../constants'
+import { formsValues, VALIDATIONS } from '../../../constants'
 import { IRestaurant } from 'src/interfaces'
+import * as yup from "yup"
 
 interface Props {
 	submitForm: (
@@ -17,9 +17,10 @@ interface Props {
 	) => Promise<void>
 	restaurant: IRestaurant
 	setFormData: React.Dispatch<React.SetStateAction<IRestaurant | null>>
-	textContent: string | null
-	setTextContent: React.Dispatch<React.SetStateAction<string | null>>
+	textContent: string
+	setTextContent: React.Dispatch<React.SetStateAction<string>>
 	update: boolean
+	preValues: IRestaurant,
 }
 
 const RestaurantMasterForm = ({
@@ -28,22 +29,51 @@ const RestaurantMasterForm = ({
 	setFormData,
 	textContent,
 	setTextContent,
-	update
+	update,
+	preValues
 }: Props) => {
 	const [open, setOpen] = useState<boolean>(false)
 	const [openAddModal, setOpenAddModal] = useState<boolean>(false)
 	const [openAddModalPdf, setOpenAddModalPdf] = useState<boolean>(false)
 	const [openModalPdf, setOpenModalPdf] = useState(false)
 	const fileInput = useRef<HTMLInputElement>(null)
-	const { locations } = useGetLocations()
-	const initialValues = generateFormValues(formsValues.restaurant, restaurant)
 
-	const { selectedFiles, handleFileSelection, setSelectedFiles } =
-		useImageState()
+	const initialValues = generateFormValues(formsValues.restaurant, restaurant)
+	const validationSchema: yup.ObjectSchema<any> = getValidationSchema()
+
+	const { data, setData, handleChange, errors, handleBlur, validate } = useFormHandling(initialValues, validationSchema)
+	const { selectedFiles, handleFileSelection, setSelectedFiles } = useImageState()
 	const { selectedFilesPdf, handleFilePdfSelection, setSelectedFilesPdf } = usePdfState()
 
+
+	const handleChangeCheckbox = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setData((prevData: IRestaurant) => ({
+			...prevData,
+			pricePerPerson: e.target.checked
+		}))
+	}
+
+	const handleSubmitForm = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault()
+		const isValid = await validate()
+		const dataSubmit: IRestaurant = data
+		dataSubmit.textContent = textContent
+		if (isValid) {
+			submitForm(dataSubmit, selectedFiles, "restaurants", update)
+		}
+	}
+
+	//seteo los valores previos para que no se renicien si el servidor manda un error 
+	useEffect(() => {
+		if (preValues) {
+			setData(preValues)
+		}
+	}, [preValues])
+
+
+
 	return (
-		<div className="flex justify-center items-center space-x-2">
+		<div className="justify-center items-center">
 			<AddPdfModal
 				open={openAddModalPdf}
 				setOpen={setOpenAddModalPdf}
@@ -81,56 +111,48 @@ const RestaurantMasterForm = ({
 				multipleCondition={true}
 				nameScreen="restaurants"
 			/>
-			<Formik
-				initialValues={initialValues}
-				onSubmit={(values) => {
-					values.textContent = textContent
-					setFormData(values)
-					submitForm(values, [...selectedFiles, ...selectedFilesPdf], 'restaurants', update)
-				}}
-				enableReinitialize
-				validationSchema={getValidationSchema()}
-			>
-				{(formik) => (
-					<div className="p-6 rounded">
-						<Form className="relative">
-							<RestaurantFormFields
-								formik={formik}
-								restaurant={restaurant}
-								setTextContent={setTextContent}
-								textContent={textContent}
-								locations={locations}
-								update={update}
-							/>
-							<div className="flex space-x-1">
-								<ShowImagesButton
-									name={true}
-									setOpen={(update && setOpen) || setOpenAddModal}
-									nameValue={update ? undefined : 'add images'}
-								>
-									{!update && (
-										<span>
-											{`${selectedFiles.length} files selected for upload`}
-										</span>
-									)}
-								</ShowImagesButton>
-								<ShowImagesButton
-									name={true}
-									setOpen={update ? setOpenModalPdf : setOpenAddModalPdf}
-									nameValue={update ? "show pdf" : 'add pdf'}
-								>
-									{!update && (
-										<span>
-											{`${selectedFilesPdf.length} files selected for upload`}
-										</span>
-									)}
-								</ShowImagesButton>
-							</div>
-						</Form>
-					</div>
-				)}
-			</Formik>
-		</div>
+			<form className='space-y-2' onSubmit={(e) => handleSubmitForm(e)}>
+				<RestaurantFormFields
+					data={data}
+					handleChange={handleChange}
+					handleChangeCheckbox={handleChangeCheckbox}
+					errors={errors}
+					handleBlur={handleBlur}
+					update={update}
+					textContent={textContent}
+					setTextContent={setTextContent}
+					restaurant={restaurant}
+				/>
+				<div className='flex justify-center items-center'>
+					<SubmitInput update={update} title='Restaurant' />
+					<ShowImagesButton
+						name={true}
+						setOpen={(update && setOpen) || setOpenAddModal}
+						nameValue={update ? undefined : 'add images'}
+					>
+						{!update && (
+							<span>
+								{`${selectedFiles.length} files selected for upload`}
+							</span>
+						)}
+					</ShowImagesButton>
+					<ShowImagesButton
+						name={true}
+						setOpen={update ? setOpenModalPdf : setOpenAddModalPdf}
+						nameValue={update ? "show pdf" : 'add pdf'}
+					>
+						{!update && (
+							<span>
+								{`${selectedFilesPdf.length} files selected for upload`}
+							</span>
+						)}
+					</ShowImagesButton>
+				</div>
+				<button type='button' onClick={() => console.log(textContent)}>
+					consola
+				</button>
+			</form >
+		</div >
 	)
 }
 
