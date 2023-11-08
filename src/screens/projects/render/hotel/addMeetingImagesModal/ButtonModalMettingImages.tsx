932f3f9594eae75e13b74c22b8851a83b05e3900
edgardo90 <edgardo9000@gmail.com>
2023-11-08 14:54:36 -0300

    103_HOTELS_PROJECT_UI_3

--- src/screens/projects/render/hotel/addMeetingImagesModal/ButtonModalMettingImages.tsx
+++ src/screens/projects/render/hotel/addMeetingImagesModal/ButtonModalMettingImages.tsx
@@ -23,7 +23,7 @@ export const ButtonModalMeetingImages: FC<ButtonModalMeetingImagesProps> = ({ ho
                 e.stopPropagation()
                 handleOpen(e)
             }}
-            style={{ position: "absolute", marginLeft: "430px" }}
+            // style={{ position: "absolute", marginLeft: "430px" }}
             className='focus:scale-110 hover:animate-pulse bg-black-50 hover:bg-orange-50 text-white-100 uppercase font-semibold hover:text-black-50 py-0 px-2 border border-orange-50 hover:border-transparent rounded'
         >
             <span style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: "16px" }} className="truncate">
