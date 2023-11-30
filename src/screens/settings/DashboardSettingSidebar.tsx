84617b38238176bd80e8b40b125ef68fb75a4351
edgardo90 <edgardo9000@gmail.com>
2023-11-29 22:31:08 -0300

    107_SETTINGS_5

--- src/screens/settings/DashboardSettingSidebar.tsx
+++ src/screens/settings/DashboardSettingSidebar.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from 'react'
+import { FC, useState, useEffect } from 'react'
 import { Icon } from '@iconify/react'
 import {
 	IDashboardData,
@@ -9,10 +9,17 @@ import { NavLink } from 'react-router-dom'
 import { useAuth } from '../../hooks'
 
 export const DashboardSettingSidebar: FC = () => {
-	const [dashboardDataList] = useState<IDashboardData[]>(dashboardDataSettings)
+	const [dashboardDataList, setDashboardDataList] = useState<IDashboardData[]>([])
 	let location = useLocation()
-
 	const { auth } = useAuth()
+
+	useEffect(() => {
+		if (auth.role === "admin") {
+			setDashboardDataList(dashboardDataSettings)
+		}
+	}, [])
+
+
 
 	return (
 		<ul className="indent-6 text-white-100 bg-black-100 h-screen mt-9 mr-5 rounded">
@@ -23,9 +30,8 @@ export const DashboardSettingSidebar: FC = () => {
 					className="font-bold text-white-50 hover:text-orange-50 border-3 border-b last:border-none border-gray-100 p-2 flex items-center cursor-pointer"
 				>
 					<NavLink
-						className={`font-bold text-white hover:text-orange-500 border-b last:border-none border-gray-100 flex items-center cursor-pointer ${
-							location.pathname == `/app/${route}` ? 'active-styles' : ''
-						}`}
+						className={`font-bold text-white hover:text-orange-500 border-b last:border-none border-gray-100 flex items-center cursor-pointer ${location.pathname == `/app/${route}` ? 'active-styles' : ''
+							}`}
 						to={`/app/${route}`}
 					>
 						<Icon icon={icon} style={{ fontSize: '22px' }} />
