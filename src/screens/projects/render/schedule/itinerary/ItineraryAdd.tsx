df56648610c0b830080b27707947e54cd3ff0381
Ezequiel <oliverm316@gmail.com>
2023-12-12 13:06:34 +0100

    Starting ItineraryAdd

--- src/screens/projects/render/schedule/itinerary/ItineraryAdd.tsx
+++ src/screens/projects/render/schedule/itinerary/ItineraryAdd.tsx
@@ -1,3 +1,16 @@
+import { Icon } from '@iconify/react'
+
 export const ItineraryAdd = () => {
-	return <p>starts in ....</p>
+	const handleClick = () => {}
+	return (
+		<div
+			className="rounded-lg cursor-pointer border-2 border-dotted border-gray-500 bg-gray-800 w-full flex items-center justify-between p-4 hover:border-orange-500 active:scale-95 transition duration-150 ease-in-out"
+			onClick={handleClick}
+		>
+			<h2 className="text-sm font-semibold text-gray-300 truncate flex flex-row items-center justify-center">
+				<Icon icon="bi:plus" width="30" className="text-orange-500 mr-2" />
+				<span className="uppercase whitespace-nowrap">Add Transfer</span>
+			</h2>
+		</div>
+	)
 }
