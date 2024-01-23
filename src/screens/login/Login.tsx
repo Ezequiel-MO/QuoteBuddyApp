05e8939c43d204fc51f898df530943bf73370062
edgardo90 <edgardo9000@gmail.com>
2024-01-23 01:52:12 -0300

    Login refactored

--- src/screens/login/Login.tsx
+++ src/screens/login/Login.tsx
@@ -1,30 +1,39 @@
-import { useEffect, useState } from 'react'
+import { FC, useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useAuth } from '../../context/auth/useAuth'
 import { Alert, Spinner } from '../../components/atoms'
 import { LoginForm } from './LoginForm'
 import { useLoginSubmit } from './useLogin'
 import { useLocalStorageItem } from 'src/hooks'
+import { LoginHeader } from './LoginHeader'
 
-export const Login = () => {
-	const [email, setEmail] = useState('')
-	const [password, setPassword] = useState('')
-	const [alert, setAlert] = useState({})
-	const [settingsLoaded, setSettingsLoaded] = useState(false)
+interface IAlert {
+	msg?: string
+	error: boolean
+}
+
+interface IUserData {
+	token: string
+	name: string
+	email: string
+}
+
+export const Login: FC = () => {
+	const [email, setEmail] = useState<string>('')
+	const [password, setPassword] = useState<string>('')
+	const [alert, setAlert] = useState<IAlert>({ error: false })
+	const [settingsLoaded, setSettingsLoaded] = useState<boolean>(false)
 
 	const { setAuth } = useAuth()
-
 	const navigate = useNavigate()
 
 	const setting = useLocalStorageItem('settings', {})
 
 	useEffect(() => {
-		if (Object.keys(setting).length > 0) {
-			setSettingsLoaded(true)
-		}
+		setSettingsLoaded(Object.keys(setting).length > 0)
 	}, [setting])
 
-	const onSuccess = (data) => {
+	const onSuccess = (data: IUserData) => {
 		localStorage.setItem('token', data.token)
 		localStorage.setItem('user_name', data.name)
 		localStorage.setItem('user_email', data.email)
@@ -40,41 +49,27 @@ export const Login = () => {
 		onSuccess
 	})
 
-	const { msg } = alert
-
 	if (!settingsLoaded) {
 		return (
-			<div className="flex flex-col items-center justify-center">
-				<Spinner />
-				<p className="text-xl mt-4 text-white-0">
-					Loading settings, the app will be ready in a few seconds...
-				</p>
-			</div>
+			<Spinner message="Loading settings, the app will be ready in a few seconds..." />
 		)
 	}
 
 	if (Object.values(setting).length === 0) {
-		return (
-			<>
-				<h1 className="font-black text-4xl capitalize">
-					<span className="text-primary">Login to APP</span>
-				</h1>
-				<Spinner />
-			</>
-		)
+		return <LoginHeader withSpinner={true} />
 	}
 
 	return (
 		<>
-			<h1 className="font-black text-4xl capitalize">
-				Login to
-				<span className="text-primary"> APP</span>
-			</h1>
+			<LoginHeader withSpinner={false} />
 			{loading ? (
 				<Spinner />
 			) : (
 				<>
-					{msg && <Alert alert={alert} />}
+					{alert.msg && (
+						<Alert alert={{ error: alert.error ?? false, msg: alert.msg }} />
+					)}
+
 					<LoginForm
 						email={email}
 						setEmail={setEmail}
---
+++ src/screens/login/LoginHeader.tsx
@@ -0,0 +1,11 @@
+import { Spinner } from '@components/atoms'
+import { FC } from 'react'
+
+export const LoginHeader: FC<{ withSpinner: boolean }> = ({ withSpinner }) => (
+	<>
+		<h1 className="font-black text-4xl capitalize">
+			Login to <span className="text-primary">APP</span>
+		</h1>
+		{withSpinner && <Spinner />}
+	</>
+)
