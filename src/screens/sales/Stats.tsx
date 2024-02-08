6c6ff60ae468c1f346ac09e4a2304a972b742835
Ezequiel <oliverm316@gmail.com>
2024-02-08 16:39:14 +0100

    Stats updated to show monthly data

--- src/screens/sales/Stats.tsx
+++ src/screens/sales/Stats.tsx
@@ -4,54 +4,59 @@ import accounting from 'accounting'
 import { listStyles } from 'src/constants/listStyles'
 
 interface ApiResponse {
-	data: IInvoiceAggregate[]
+	data: IMonthlyAggregate[]
 }
 
-interface IInvoiceAggregate {
-	year: string
-	currency?: 'EUR' | 'USD'
+interface IMonthlyAggregate {
+	_id: {
+		month: number
+		year: number
+		currency: 'EUR' | 'USD'
+	}
 	totalAmount: number
 }
 
-interface IYearlyAmounts {
-	EUR: number
-	USD: number
+interface MonthlyTotals {
+	[key: string]: number // key format: "2023-1-EUR"
 }
 
-export const Stats: React.FC = () => {
-	const [data, setData] = useState<Record<string, IYearlyAmounts>>({})
-
-	const processData = (invoiceData: IInvoiceAggregate[]) => {
-		const currentYear = new Date().getFullYear()
-		const relevantYears = [String(currentYear), String(currentYear - 1)]
-
-		const initialProcessedData: Record<string, IYearlyAmounts> = {}
-
-		const processedData = invoiceData.reduce(
-			(acc: Record<string, IYearlyAmounts>, curr) => {
-				if (!relevantYears.includes(curr.year) || !curr.currency) return acc
-				if (!acc[curr.year]) acc[curr.year] = { EUR: 0, USD: 0 }
-				acc[curr.year][curr.currency] += curr.totalAmount
-
-				return acc
-			},
-			initialProcessedData
-		)
-
-		relevantYears.forEach((year) => {
-			if (!processedData[year]) processedData[year] = { EUR: 0, USD: 0 }
-		})
-
-		setData(processedData)
+interface AccumulatedTotals {
+	[currency: string]: {
+		[year: number]: number[]
 	}
+}
+
+const monthNames = [
+	'JAN',
+	'FEB',
+	'MAR',
+	'APR',
+	'MAY',
+	'JUN',
+	'JUL',
+	'AUG',
+	'SEP',
+	'OCT',
+	'NOV',
+	'DEC'
+]
+
+export const Stats: React.FC = () => {
+	const [monthlyTotals, setMonthlyTotals] = useState<MonthlyTotals>({})
+	const [accumulatedTotals, setAccumulatedTotals] = useState<AccumulatedTotals>(
+		{
+			EUR: {},
+			USD: {}
+		}
+	)
 
 	useEffect(() => {
 		const fetchInvoiceData = async () => {
 			try {
-				const response = await baseAPI.get<ApiResponse>(
-					'invoices/aggregate-by-year'
+				const { data } = await baseAPI.get<ApiResponse>(
+					'invoices/aggregate-by-month'
 				)
-				processData(response.data.data)
+				processInvoiceData(data.data)
 			} catch (error) {
 				console.error('Error fetching invoice data:', error)
 			}
@@ -60,42 +65,88 @@ export const Stats: React.FC = () => {
 		fetchInvoiceData()
 	}, [])
 
+	const processInvoiceData = (data: IMonthlyAggregate[]) => {
+		const monthly: MonthlyTotals = {}
+		const accumulated: AccumulatedTotals = { EUR: {}, USD: {} }
+
+		data.forEach(({ _id: { month, year, currency }, totalAmount }) => {
+			const key = `${year}-${month}-${currency}`
+			monthly[key] = (monthly[key] || 0) + totalAmount
+
+			accumulated[currency][year] =
+				accumulated[currency][year] || Array(12).fill(0)
+			accumulated[currency][year][month - 1] += totalAmount
+		})
+
+		// Correctly type the parameters in the forEach method to accumulate totals
+		Object.keys(accumulated).forEach((currency) => {
+			Object.keys(accumulated[currency]).forEach((yearStr) => {
+				const year = Number(yearStr) // Ensure year is treated as a number
+				accumulated[currency][year].forEach(
+					(value: number, index: number, arr: number[]) => {
+						arr[index] = index === 0 ? value : arr[index - 1] + value
+					}
+				)
+			})
+		})
+
+		setMonthlyTotals(monthly)
+		setAccumulatedTotals(accumulated)
+	}
+
 	const currentYear = new Date().getFullYear()
-	const relevantYears = [String(currentYear), String(currentYear - 1)]
+	const previousYear = currentYear - 1
 
 	return (
 		<div className="overflow-x-auto">
-			<h2 className="mb-2 uppercase font-bold underline">
-				Total Invoiced - Year To Date
-			</h2>
 			<table className={listStyles.table}>
-				<thead className={listStyles.thead}>
-					<tr className={listStyles.tr}>
-						<th className={listStyles.th}>Currency</th>
-						{relevantYears.map((year) => (
-							<th key={year} className={listStyles.th}>
-								{year}
-							</th>
-						))}
+				<thead>
+					<tr>
+						<th className={listStyles.th}>Month/Currency</th>
+						<th className={listStyles.th}>{previousYear}</th>
+						<th className={listStyles.th}>{currentYear}</th>
+						<th className={listStyles.th}>{previousYear}-YTD</th>
+						<th className={listStyles.th}>{currentYear}-YTD</th>
 					</tr>
 				</thead>
-				<tbody className={listStyles.tbody}>
-					{['EUR', 'USD'].map((currency) => (
-						<tr key={currency} className={listStyles.tr}>
-							<td className={listStyles.td}>{currency}</td>
-							{relevantYears.map((year) => (
-								<td key={year} className={listStyles.td}>
-									{data[year] &&
-									data[year][currency as keyof IYearlyAmounts] !== undefined
-										? accounting.formatMoney(
-												data[year][currency as keyof IYearlyAmounts],
-												currency === 'EUR' ? '€ ' : '$ '
-										  )
-										: '0.00'}
+				<tbody>
+					{['EUR', 'USD'].flatMap((currency) =>
+						monthNames.map((month, index) => (
+							<tr key={`${month}-${currency}`}>
+								<td className={listStyles.td}>{`${month}.${currency}`}</td>
+								{/* Display monthly total for previous year */}
+								<td className={listStyles.td}>
+									{accounting.formatMoney(
+										monthlyTotals[`${previousYear}-${index + 1}-${currency}`] ||
+											0,
+										currency === 'EUR' ? '€' : '$'
+									)}
 								</td>
-							))}
-						</tr>
-					))}
+								{/* Display monthly total for current year */}
+								<td className={listStyles.td}>
+									{accounting.formatMoney(
+										monthlyTotals[`${currentYear}-${index + 1}-${currency}`] ||
+											0,
+										currency === 'EUR' ? '€' : '$'
+									)}
+								</td>
+								{/* Display YTD total for previous year */}
+								<td className={listStyles.td}>
+									{accounting.formatMoney(
+										accumulatedTotals[currency][previousYear]?.[index] || 0,
+										currency === 'EUR' ? '€' : '$'
+									)}
+								</td>
+								{/* Display YTD total for current year */}
+								<td className={listStyles.td}>
+									{accounting.formatMoney(
+										accumulatedTotals[currency][currentYear]?.[index] || 0,
+										currency === 'EUR' ? '€' : '$'
+									)}
+								</td>
+							</tr>
+						))
+					)}
 				</tbody>
 			</table>
 		</div>
