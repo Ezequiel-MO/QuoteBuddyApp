b809baf2be770b145270abe87ad3fa269ab6a366
Ezequiel <oliverm316@gmail.com>
2024-04-11 13:31:30 +0200

    TopBar isLoading signals if pdf is being generated

--- src/client/TopBar.tsx
+++ src/client/TopBar.tsx
@@ -4,8 +4,15 @@ import Modal from '@components/organisms/Modal'
 import OverviewTable from '@screens/clientMainPage/overview/OverviewTable'
 import { MapWrapper } from '@screens/vendor_map/Wrapper'
 import { Destination } from './destination/Destination'
+import baseAPI from 'src/axios/axiosConfig'
+import { useCurrentProject } from 'src/hooks'
+import { useNavigate } from 'react-router-dom'
+import { Spinner } from '@components/atoms'
 
 const TopBar: React.FC = () => {
+	const { currentProject } = useCurrentProject()
+	const navigate = useNavigate()
+	const [isLoading, setIsLoading] = useState(false)
 	const [modalContent, setModalContent] = useState<React.ReactNode>(null)
 	const [modalType, setModalType] = useState<
 		'closed' | 'destination' | 'overview' | 'map'
@@ -21,35 +28,77 @@ const TopBar: React.FC = () => {
 		setModalContent(content)
 	}
 
+	const handleGeneratePDF = async () => {
+		setIsLoading(true)
+		try {
+			const response = await baseAPI.post(
+				'generate-pdf',
+				JSON.stringify(currentProject),
+				{
+					responseType: 'blob'
+				}
+			)
+			const pdfBlob = new Blob([response.data], { type: 'application/pdf' })
+			const pdfUrl = URL.createObjectURL(pdfBlob)
+
+			navigate('/client/pdf', { state: { pdfUrl } })
+		} catch (error) {
+			console.error('Error generating PDF:', error)
+		} finally {
+			setIsLoading(false)
+		}
+	}
+
 	return (
 		<div className="flex justify-between items-center dark:text-white-0 p-4">
-			<div className="flex space-x-4">
-				<div
-					className="flex flex-col items-center cursor-pointer"
-					onClick={() => openModalWithContent(<Destination />, 'destination')}
-				>
-					<Icon icon="bx:world" width="30" color="#ea5933" />
-					<span>Destination</span>
+			{isLoading ? (
+				<div style={{ textAlign: 'center' }}>
+					<Spinner />
+					<p>Bear with me, your PDF is being generated ...</p>
 				</div>
-				<div
-					className="flex flex-col items-center cursor-pointer"
-					onClick={() => openModalWithContent(<MapWrapper />, 'map')}
-				>
-					<Icon icon="akar-icons:map" width="30" color="#ea5933" />
-					<span>Map</span>
-				</div>
-				<div
-					className="flex flex-col items-center cursor-pointer"
-					onClick={() => openModalWithContent(<OverviewTable />, 'overview')}
-				>
-					<Icon icon="bi:calendar-check" width="30" color="#ea5933" />
-					<span>Overview</span>
-				</div>
-				<div className="flex flex-col items-center cursor-pointer">
-					<Icon icon="ant-design:printer-twotone" width="30" color="#ea5933" />
-					<span>PDF</span>
-				</div>
-			</div>
+			) : (
+				<>
+					<div className="flex space-x-4">
+						<div
+							className="flex flex-col items-center cursor-pointer"
+							onClick={() =>
+								openModalWithContent(<Destination />, 'destination')
+							}
+						>
+							<Icon icon="bx:world" width="30" color="#ea5933" />
+							<span>Destination</span>
+						</div>
+						<div
+							className="flex flex-col items-center cursor-pointer"
+							onClick={() => openModalWithContent(<MapWrapper />, 'map')}
+						>
+							<Icon icon="akar-icons:map" width="30" color="#ea5933" />
+							<span>Map</span>
+						</div>
+						<div
+							className="flex flex-col items-center cursor-pointer"
+							onClick={() =>
+								openModalWithContent(<OverviewTable />, 'overview')
+							}
+						>
+							<Icon icon="bi:calendar-check" width="30" color="#ea5933" />
+							<span>Overview</span>
+						</div>
+						<div
+							onClick={handleGeneratePDF}
+							className="flex flex-col items-center cursor-pointer"
+						>
+							<Icon
+								icon="ant-design:printer-twotone"
+								width="30"
+								color="#ea5933"
+							/>
+							<span>PDF</span>
+						</div>
+					</div>
+				</>
+			)}
+
 			<Modal
 				isOpen={isModalOpen}
 				modalType={modalType}
--- src/screens/clientMainPage/pdf/PDFPresentation.tsx
+++ src/screens/clientMainPage/pdf/PDFPresentation.tsx
@@ -1,39 +1,19 @@
-import { StyleSheet, PDFViewer } from '@react-pdf/renderer'
-import PDFMainComponent from './PDFMainComponent'
-import { useCurrentProject } from 'src/hooks'
-
-export const styles = StyleSheet.create({
-	page: {
-		flexDirection: 'column',
-		backgroundColor: '#E4E4E4'
-	},
-	section: {
-		margin: 10,
-		padding: 10,
-		flexGrow: 1
-	},
-	viewer: {
-		width: '100%',
-		height: '100vh',
-		maxWidth: '1400px',
-		margin: '0 auto'
-	},
-
-	quoteTitle: {
-		fontSize: 18,
-		fontWeight: 'extrabold',
-		textAlign: 'center',
-		color: '#4B5563',
-		marginBottom: 24
-	}
-})
+import { useLocation } from 'react-router-dom'
 
 const PDFPresentation = () => {
-	const { currentProject } = useCurrentProject()
+	const location = useLocation()
+	const pdfUrl = location.state?.pdfUrl
+
+	if (!pdfUrl) {
+		return <div>No PDF file found.</div>
+	}
+
 	return (
-		<PDFViewer style={styles.viewer}>
-			<PDFMainComponent currentProject={currentProject} />
-		</PDFViewer>
+		<iframe
+			src={pdfUrl}
+			style={{ width: '100%', height: '100vh', border: 'none' }}
+			title="PDF Viewer"
+		></iframe>
 	)
 }
 
