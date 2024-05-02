259c0449f3a5a425dcf41410a9e291af3c8c5e88
Ezequiel <oliverm316@gmail.com>
2024-05-02 13:21:37 +0200

    minor fix in GiftSection

--- src/screens/budget/MainTable/rows/gift/GiftSection.tsx
+++ src/screens/budget/MainTable/rows/gift/GiftSection.tsx
@@ -1,32 +1,29 @@
-import { useState , useEffect } from 'react'
-import { IGift } from "src/interfaces/"
+import { useState, useEffect } from 'react'
+import { IGift } from 'src/interfaces/'
 import { useContextBudget } from '../../../context/BudgetContext'
-import { GiftRow } from "./GiftRow"
-
-
+import { GiftRow } from './GiftRow'
 
 export const GiftSection = () => {
-    const { state, dispatch } = useContextBudget()
-    const [selectedGift, setSelectedGift] = useState<IGift>(state.gifts[0])
+	const { state, dispatch } = useContextBudget()
+	if (state.gifts.length === 0) return null
+	const [selectedGift, setSelectedGift] = useState<IGift>(state.gifts[0])
 
-    if (state.gifts.length === 0) return null
+	useEffect(() => {
+		dispatch({
+			type: 'UPDATE_GIFT_COST',
+			payload: {
+				value: selectedGift.qty * selectedGift.price
+			}
+		})
+	}, [state.gifts, selectedGift, dispatch])
 
-    useEffect(()=>{
-        dispatch({
-            type:"UPDATE_GIFT_COST",
-            payload:{
-                value: selectedGift.qty * selectedGift.price
-            }
-        })
-    },[state.gifts , selectedGift , dispatch])
-
-    return (
-        <>
-            <GiftRow
-                items={state.gifts}
-                selectedGift={selectedGift}
-                setSelectedGift={setSelectedGift}
-            />
-        </>
-    )
+	return (
+		<>
+			<GiftRow
+				items={state.gifts}
+				selectedGift={selectedGift}
+				setSelectedGift={setSelectedGift}
+			/>
+		</>
+	)
 }
