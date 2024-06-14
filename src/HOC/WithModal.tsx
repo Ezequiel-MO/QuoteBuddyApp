2b13e2057c01a9d367288d86384443120f1750a4
Ezequiel <oliverm316@gmail.com>
2024-06-14 15:55:47 +0200

    clicking outside the hotels modal, it also works

--- src/HOC/WithModal.tsx
+++ src/HOC/WithModal.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { Icon } from '@iconify/react'
 
 interface ModalProps {
@@ -11,13 +11,34 @@ const withModal = <P extends object>(
 	WrappedComponent: React.ComponentType<P>
 ): React.FC<P & ModalProps> => {
 	return ({ isOpen, onClose, title, ...props }: ModalProps) => {
+		const modalRef = useRef<HTMLDivElement>(null)
+
+		const handleClickOutside = (event: MouseEvent) => {
+			if (
+				modalRef.current &&
+				!modalRef.current.contains(event.target as Node)
+			) {
+				onClose()
+			}
+		}
+
+		useEffect(() => {
+			document.addEventListener('mousedown', handleClickOutside)
+			return () => {
+				document.removeEventListener('mousedown', handleClickOutside)
+			}
+		}, [])
+
 		if (!isOpen) {
 			return null
 		}
 
 		return (
 			<div className="fixed inset-0 bg-gray-700 bg-opacity-75 flex justify-center items-center z-[999]">
-				<div className="bg-slate-800 p-8 rounded-lg shadow-lg relative w-full max-w-2xl mx-4 z-50">
+				<div
+					ref={modalRef}
+					className="bg-slate-800 p-8 rounded-lg shadow-lg relative w-full max-w-2xl mx-4 z-50"
+				>
 					<button
 						onClick={onClose}
 						className="absolute top-2 right-2 text-gray-600 hover:text-gray-900"
