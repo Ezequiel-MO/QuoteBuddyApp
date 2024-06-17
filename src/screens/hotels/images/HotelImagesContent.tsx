9ab1b933b6c4b841f6359e9e650be5fa80481b85
Ezequiel <oliverm316@gmail.com>
2024-06-17 10:54:09 +0200

    Images can be deleted now

--- src/screens/hotels/images/HotelImagesContent.tsx
+++ src/screens/hotels/images/HotelImagesContent.tsx
@@ -4,20 +4,6 @@ import Thumbnail from '@components/molecules/Thumbnail'
 import baseAPI from 'src/axios/axiosConfig'
 import { toast } from 'react-toastify'
 import { errorToastOptions } from 'src/helper/toast'
-import { DndContext, closestCenter } from '@dnd-kit/core'
-import {
-	arrayMove,
-	SortableContext,
-	sortableKeyboardCoordinates,
-	verticalListSortingStrategy
-} from '@dnd-kit/sortable'
-import { SortableItem } from './SortableItem' // You'll create this component next
-import {
-	useSensor,
-	useSensors,
-	PointerSensor,
-	KeyboardSensor
-} from '@dnd-kit/core'
 
 const HotelImagesContent: React.FC = () => {
 	const [loading, setLoading] = useState(false)
@@ -91,71 +77,19 @@ const HotelImagesContent: React.FC = () => {
 		}
 	}
 
-	const handleDragEnd = (event: any) => {
-		const { active, over } = event
-		if (active.id !== over.id) {
-			const oldIndex = state.currentHotel?.imageContentUrl?.findIndex(
-				(url) => url === active.id
-			)
-			const newIndex = state.currentHotel?.imageContentUrl?.findIndex(
-				(url) => url === over.id
-			)
-			if (
-				oldIndex !== undefined &&
-				newIndex !== undefined &&
-				oldIndex !== -1 &&
-				newIndex !== -1
-			) {
-				const updatedImageUrls = arrayMove(
-					state.currentHotel?.imageContentUrl || [],
-					oldIndex,
-					newIndex
-				)
-				dispatch({
-					type: 'UPDATE_HOTEL_FIELD',
-					payload: {
-						name: 'imageContentUrl',
-						value: updatedImageUrls
-					}
-				})
-			}
-		}
-	}
-
-	const sensors = useSensors(
-		useSensor(PointerSensor),
-		useSensor(KeyboardSensor, {
-			coordinateGetter: sortableKeyboardCoordinates
-		})
-	)
-
 	return (
-		<DndContext
-			sensors={sensors}
-			collisionDetection={closestCenter}
-			onDragEnd={handleDragEnd}
-		>
-			<SortableContext
-				items={state.currentHotel?.imageContentUrl || []}
-				strategy={verticalListSortingStrategy}
-			>
-				<div className="grid grid-cols-3 sm:grid-cols-4 gap-2">
-					{(state.currentHotel?.imageContentUrl || []).map(
-						(imageSrc, index) => (
-							<SortableItem
-								key={imageSrc}
-								id={imageSrc}
-								imageSrc={imageSrc}
-								onDelete={() => handleImageDelete(index)}
-							/>
-						)
-					)}
-					{(state.currentHotel?.imageContentUrl || []).length < 12 && (
-						<Thumbnail onImageUpload={handleImageUpload} isLoading={loading} />
-					)}
-				</div>
-			</SortableContext>
-		</DndContext>
+		<div className="grid grid-cols-3 sm:grid-cols-4 gap-2">
+			{(state.currentHotel?.imageContentUrl || []).map((imageSrc, index) => (
+				<Thumbnail
+					key={imageSrc}
+					imageSrc={imageSrc}
+					onDelete={() => handleImageDelete(index)}
+				/>
+			))}
+			{(state.currentHotel?.imageContentUrl || []).length < 12 && (
+				<Thumbnail onImageUpload={handleImageUpload} isLoading={loading} />
+			)}
+		</div>
 	)
 }
 
