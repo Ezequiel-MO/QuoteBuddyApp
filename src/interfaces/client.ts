0209dcc3847d9a0339c7c153f6c108ec36393783
Ezequiel <oliverm316@gmail.com>
2024-08-07 17:07:36 +0200

    minor fix

--- src/interfaces/client.ts
+++ src/interfaces/client.ts
@@ -1,10 +1,21 @@
 interface IClientOrigin {
-	method: 'Recommendation' | 'ClientChangedCompany' | 'Tradeshow' | 'ColdCall' | 'TourismPatronat' | 'Other'
+	method:
+		| 'Recommendation'
+		| 'ClientChangedCompany'
+		| 'Tradeshow'
+		| 'ColdCall'
+		| 'TourismPatronat'
+		| 'Other'
 	textContent?: string
 }
 
 interface IClientQualification {
-	status: 'NeverRequested' | 'RequestedButNotProceeded' | 'Proceeded' | 'RegularClient' | 'LostClient'
+	status:
+		| 'NeverRequested'
+		| 'RequestedButNotProceeded'
+		| 'Proceeded'
+		| 'RegularClient'
+		| 'LostClient'
 	textContent?: string
 }
 
@@ -14,7 +25,6 @@ export interface IClientNote {
 	textContent: string
 }
 
-
 export interface IClient {
 	_id?: string
 	firstName: string
@@ -23,17 +33,17 @@ export interface IClient {
 	phone?: string
 	country?: string
 	quoteLanguage:
-	| 'EN'
-	| 'FR'
-	| 'IT'
-	| 'ES'
-	| 'DE'
-	| 'NL'
-	| 'BE'
-	| 'RO'
-	| 'DK'
-	| 'SE'
-	clientCompany?: string
+		| 'EN'
+		| 'FR'
+		| 'IT'
+		| 'ES'
+		| 'DE'
+		| 'NL'
+		| 'BE'
+		| 'RO'
+		| 'DK'
+		| 'SE'
+	clientCompany?: IClient | string
 	origin: IClientOrigin
 	qualification: IClientQualification
 	clientNotes?: IClientNote[]
