7605a73c24d2b9755fac4c6a612d310b5ad8df86
Ezequiel <oliverm316@gmail.com>
2024-09-03 11:10:44 +0200

    helper functions correctly typed

---
+++ src/helper/helperFunctions.ts
@@ -0,0 +1,58 @@
+import { IInvoiceBreakdownLine } from '@interfaces/invoice'
+
+export const computeTotalDays = (
+	startDate: string,
+	endDate: string
+): number => {
+	const start = new Date(startDate)
+	const end = new Date(endDate)
+	const diffTime = Math.abs(end.getTime() - start.getTime())
+	const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)) + 1
+	return diffDays
+}
+
+export const whichDay = (counter: number, daydifference: number): string => {
+	if (counter === 1) {
+		return 'Arrival Day'
+	} else if (counter === daydifference) {
+		return 'Departure Day'
+	} else {
+		return 'Day ' + counter
+	}
+}
+
+export const computeInvoiceBreakdownTotal = (
+	invoiceBreakdown: IInvoiceBreakdownLine[]
+): number => {
+	if (invoiceBreakdown.length === 1) {
+		return 0
+	} else {
+		let total = 0
+		for (let i = 1; i < invoiceBreakdown.length; i++) {
+			total += Number(invoiceBreakdown[i].amount)
+		}
+		return total
+	}
+}
+
+export function formatCamelCaseToWords(nameEvent: string): string {
+	const upLetter = nameEvent
+		.split('')
+		.find((el) => el === el.toLocaleUpperCase())
+	const indexUpLetter = nameEvent.indexOf(upLetter || '')
+	if (indexUpLetter === -1) return nameEvent
+	return (
+		nameEvent.slice(0, indexUpLetter) +
+		' ' +
+		nameEvent.slice(indexUpLetter).toLowerCase()
+	)
+}
+
+export function findPathname(
+	pathname: string = '',
+	routes: string[] = []
+): boolean {
+	const path = pathname.split('/').slice(0, 3).join('')
+	const route = routes.filter((el) => el.split('/').join('').includes(path))
+	return route.length > 0
+}
