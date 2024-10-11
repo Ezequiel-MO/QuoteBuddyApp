2dbd3d0e25bd15c7c1da58af83086235c70c8b31
edgardo90 <edgardo9000@gmail.com>
2024-10-11 02:30:32 -0300

    Add Budget PDF to  Project form

--- src/screens/projects/images/ProjectImagesContent.tsx
+++ src/screens/projects/images/ProjectImagesContent.tsx
@@ -1,34 +1,26 @@
 import React, { useState } from 'react'
-import Thumbnail from '@components/molecules/Thumbnail'
 import baseAPI from 'src/axios/axiosConfig'
 import { toast } from 'react-toastify'
-import { errorToastOptions } from 'src/helper/toast'
-import { DndContext, closestCenter } from '@dnd-kit/core'
-import {
-	arrayMove,
-	SortableContext,
-	sortableKeyboardCoordinates,
-	verticalListSortingStrategy
-} from '@dnd-kit/sortable'
-import { useSensor, useSensors, KeyboardSensor } from '@dnd-kit/core'
-import CustomPointerSensor from 'src/helper/dragndrop/CustomPointerSensor'
-import { SortableItem } from 'src/helper/dragndrop/SortableItem'
+import { errorToastOptions, toastOptions } from 'src/helper/toast'
 import { useProject } from '../context/ProjectContext'
+import { ThumbnailPDF } from "src/components/molecules/ThumbnailPDF"
+import { useCurrentProject } from 'src/hooks'
+
 
 const ProjectImagesContent: React.FC = () => {
 	const [loading, setLoading] = useState(false)
 	const { state, dispatch } = useProject()
+	const { currentProject, setCurrentProject, deletedBudgetPDF, addBudgetPDF } = useCurrentProject()
 
 	const handleImageUpload = async (file: File) => {
 		setLoading(true)
 		const formData = new FormData()
 		formData.append('imageContentUrl', file)
-
 		try {
 			let newImageUrls: string[] = []
-			if (state.update && state.currentProject?._id) {
+			if (state.update && currentProject?._id) {
 				const response = await baseAPI.patch(
-					`projects/images/${state.currentProject._id}`,
+					`projects/images/${currentProject._id}`,
 					formData,
 					{
 						headers: {
@@ -41,14 +33,8 @@ const ProjectImagesContent: React.FC = () => {
 				const blobUrl = URL.createObjectURL(file)
 				newImageUrls = [blobUrl]
 			}
-
-			dispatch({
-				type: 'APPEND_TO_ARRAY_FIELD',
-				payload: {
-					name: 'imageContentUrl',
-					value: newImageUrls
-				}
-			})
+			addBudgetPDF(newImageUrls)
+			state.searchTerm = "" //para reniciar el listado
 		} catch (error: any) {
 			console.error('Image upload failed:', error)
 			alert('Image upload failed. Please try again.')
@@ -57,96 +43,48 @@ const ProjectImagesContent: React.FC = () => {
 		}
 	}
 
-	const handleImageDelete = async () => {
-		if (!state.currentProject?.imageContentUrl) return
-
+	const handleImageDelete = async (urlPdf: string) => {
+		if (!currentProject?.imageContentUrl) {
+			return
+		}
+		const loadingToast = toast.loading('Delete PDF, please wait')
+		let response
+		const data = { pdfUrl: urlPdf , keyDoc:"imageContentUrl" }
 		try {
-			if (state.update && state.currentProject._id) {
-				await baseAPI.delete(`projects/images/${state.currentProject._id}`, {
-					data: {
-						pdfUrl: state.currentProject?.imageContentUrl[0]
-					}
-				})
+			if (state.update && currentProject._id) {
+				response = await baseAPI.delete(`projects/images/${currentProject._id}`, { data })
 			}
-
-			dispatch({
-				type: 'UPDATE_PROJECT_FIELD',
-				payload: {
-					name: 'imageContentUrl',
-					value: []
-				}
-			})
+			toast.dismiss(loadingToast)
+			deletedBudgetPDF(urlPdf)
+			toast.success('Deleted PDF successfully', toastOptions)
+			state.searchTerm = "" //para reniciar el listado
 		} catch (error: any) {
+			toast.dismiss(loadingToast)
 			console.error('Image deletion failed:', error)
 			toast.error('Image deletion failed.', errorToastOptions)
 		}
 	}
 
-	const handleDragEnd = (event: any) => {
-		const { active, over } = event
-		if (active.id !== over.id) {
-			const oldIndex = state.currentProject?.imageContentUrl?.findIndex(
-				(url) => url === active.id
-			)
-			const newIndex = state.currentProject?.imageContentUrl?.findIndex(
-				(url) => url === over.id
-			)
-			if (
-				oldIndex !== undefined &&
-				newIndex !== undefined &&
-				oldIndex !== -1 &&
-				newIndex !== -1
-			) {
-				const updatedImageUrls = arrayMove(
-					state.currentProject?.imageContentUrl || [],
-					oldIndex,
-					newIndex
-				)
-				dispatch({
-					type: 'UPDATE_PROJECT_FIELD',
-					payload: {
-						name: 'imageContentUrl',
-						value: updatedImageUrls
-					}
-				})
-			}
-		}
-	}
 
-	const sensors = useSensors(
-		useSensor(CustomPointerSensor),
-		useSensor(KeyboardSensor, {
-			coordinateGetter: sortableKeyboardCoordinates
-		})
-	)
 
 	return (
-		<DndContext
-			sensors={sensors}
-			collisionDetection={closestCenter}
-			onDragEnd={handleDragEnd}
-		>
-			<SortableContext
-				items={state.currentProject?.imageContentUrl || []}
-				strategy={verticalListSortingStrategy}
-			>
-				<div className="grid grid-cols-3 sm:grid-cols-4 gap-2">
-					{(state.currentProject?.imageContentUrl || []).map(
-						(imageSrc, index) => (
-							<SortableItem
-								key={imageSrc}
-								id={imageSrc}
-								imageSrc={imageSrc}
-								onDelete={handleImageDelete}
-							/>
-						)
-					)}
-					{(state.currentProject?.imageContentUrl || []).length < 1 && (
-						<Thumbnail onImageUpload={handleImageUpload} isLoading={loading} />
-					)}
-				</div>
-			</SortableContext>
-		</DndContext>
+		<>
+			<div className="grid grid-cols-3 sm:grid-cols-4 gap-2">
+				{
+					(currentProject?.imageContentUrl || []).map((pdfSrc, index) => (
+						<ThumbnailPDF
+							imageSrc={pdfSrc}
+							onDelete={() => handleImageDelete(pdfSrc)}
+							onImageUpload={() => { }}
+							key={index}
+						/>
+					))
+				}
+				{(currentProject?.imageContentUrl || []).length < 1 && (
+					<ThumbnailPDF onImageUpload={handleImageUpload} isLoading={loading} />
+				)}
+			</div>
+		</>
 	)
 }
 
